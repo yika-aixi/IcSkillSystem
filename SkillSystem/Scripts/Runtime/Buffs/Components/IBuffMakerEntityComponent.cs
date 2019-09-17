bc1f9fb6f57360f28265a5b4a0fc07478ed05156
Icarus <1375400884@qq.com>
2019-09-17 17:59:44 +0800

    !A(Buff Component) 标记组件

---
+++ SkillSystem/Scripts/Runtime/Buffs/Components/IBuffMakerEntityComponent.cs
@@ -0,0 +1,9 @@
+﻿using Cabin_Icarus.SkillSystem.Scripts.Runtime.Buffs.Entitys;
+
+namespace CabinIcarus.SkillSystem.Runtime.Buffs.Components
+{
+    public interface IBuffMakerEntityComponent
+    {
+        IEntity Maker { get; set; }
+    }
+}
