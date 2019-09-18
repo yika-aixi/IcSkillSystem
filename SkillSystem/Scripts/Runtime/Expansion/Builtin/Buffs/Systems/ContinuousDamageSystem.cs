e2daef54ca59cb89b2455d20622e957bd79abeee
Icarus <1375400884@qq.com>
2019-09-18 18:06:34 +0800

    !A 3个Buff逻辑系统

---
+++ SkillSystem/Scripts/Runtime/Expansion/Builtin/Buffs/Systems/ContinuousDamageSystem.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using CabinIcarus.IcSkillSystem.Expansion.Runtime.Buffs.Components;
+using CabinIcarus.IcSkillSystem.Runtime.Buffs;
+using CabinIcarus.IcSkillSystem.Runtime.Buffs.Components;
+using CabinIcarus.IcSkillSystem.Runtime.Buffs.Entitys;
+using CabinIcarus.IcSkillSystem.Runtime.Buffs.Systems;
+using CabinIcarus.IcSkillSystem.Runtime.Buffs.Systems.Interfaces;
+using UnityEngine;
+
+namespace CabinIcarus.IcSkillSystem.Expansion.Runtime.Builtin.Buffs.Systems
+{
+    /// <summary>
+    /// 持续伤害
+    /// </summary>
+    public class ContinuousDamageSystem<T>:ABuffUpdateSystem,IBuffCreateSystem where T : IDamageBuff,new()
+    {
+        private List<IContinuousDamageBuff> _continuousBuffs;
+        public ContinuousDamageSystem(IBuffManager buffManager) : base(buffManager)
+        {
+            _continuousBuffs = new List<IContinuousDamageBuff>();
+        }
+        public override bool Filter(IEntity entity)
+        {
+            return BuffManager.HasBuff<IMechanicBuff>(entity,x=>x.MechanicsType == MechanicsType.Health);
+        }
+        public override void Execute(IEntity entity)
+        {
+            BuffManager.GetBuffs(entity,_continuousBuffs);
+            for (var i = 0; i < _continuousBuffs.Count; i++)
+            {
+                var buff = _continuousBuffs[i];
+                buff.Duration -= Time.deltaTime;
+                if (buff.Duration > 0)
+                {
+                    if (buff.LastTriggerTime - buff.Duration >= buff.TriggerInterval)
+                    {
+                        buff.LastTriggerTime = buff.Duration;
+                        
+                        BuffManager.AddBuff(entity,new T()
+                        {
+                            Maker = buff.Maker,
+                            Type = buff.Type,
+                            Value = buff.Value
+                        });
+                    }
+                }
+                else
+                {
+                    BuffManager.RemoveBuff(entity,buff);
+                }
+            }
+        }
+
+        #region Init 
+
+        public bool Filter(IEntity entity, IBuffDataComponent buff)
+        {
+            return BuffManager.HasBuff<IMechanicBuff>(entity,x=>x.MechanicsType == MechanicsType.Health) && buff is IContinuousDamageBuff;
+        }
+        public void Create(IEntity entity, IBuffDataComponent buff)
+        {
+            IContinuousDamageBuff continuousDamageBuff = (IContinuousDamageBuff) buff;
+            continuousDamageBuff.LastTriggerTime = continuousDamageBuff.Duration;
+        }
+
+        #endregion
+    }
+}
---
+++ SkillSystem/Scripts/Runtime/Expansion/Builtin/Buffs/Systems/DamageReducePercentageSystem.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using CabinIcarus.IcSkillSystem.Expansion.Runtime.Buffs.Components;
+using CabinIcarus.IcSkillSystem.Runtime.Buffs;
+using CabinIcarus.IcSkillSystem.Runtime.Buffs.Components;
+using CabinIcarus.IcSkillSystem.Runtime.Buffs.Entitys;
+using CabinIcarus.IcSkillSystem.Runtime.Buffs.Systems;
+
+namespace CabinIcarus.IcSkillSystem.Expansion.Runtime.Builtin.Buffs.Systems
+{
+    /// <summary>
+    /// 百分比伤害减少,如果存在多个一样的buff,只会使用最后一个
+    /// </summary>
+    public class DamageReducePercentageSystem:ABuffCreateSystem
+    {
+        private List<IDamageReducePercentageBuff> _damageReducePercentageBuffs;
+        public DamageReducePercentageSystem(IBuffManager buffManager) : base(buffManager)
+        {
+            _damageReducePercentageBuffs = new List<IDamageReducePercentageBuff>();
+        }
+
+        public override bool Filter(IEntity entity, IBuffDataComponent buff)
+        {
+            return buff is IDamageBuff && BuffManager.HasBuff<IDamageReducePercentageBuff>(entity) && BuffManager.HasBuff<IMechanicBuff>(entity,x=>x.MechanicsType == MechanicsType.Health);
+        }
+
+        public override void Create(IEntity entity, IBuffDataComponent buff)
+        {
+            BuffManager.GetBuffs(entity,_damageReducePercentageBuffs);
+
+            //todo 只使用最后一个减伤buff
+            IDamageReducePercentageBuff damageReduceFixedBuff = _damageReducePercentageBuffs[_damageReducePercentageBuffs.Count - 1];
+
+            IDamageBuff damageBuff = (IDamageBuff) buff;
+            
+            if (damageBuff.Type == damageReduceFixedBuff.Type)
+            {
+                damageBuff.Value -= damageBuff.Value * damageReduceFixedBuff.Value;
+            }
+        }
+    }
+}
