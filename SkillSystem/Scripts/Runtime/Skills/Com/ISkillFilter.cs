09ff2c34ed7567439b3db45e382bf4d283ba12b0
Icarus <1375400884@qq.com>
2019-09-19 00:13:46 +0800

    !A 技能过滤接口

---
+++ SkillSystem/Scripts/Runtime/Skills/Com/ISkillFilter.cs
@@ -0,0 +1,22 @@
+//创建者:Icarus
+//手动滑稽,滑稽脸
+//ヾ(•ω•`)o
+//https://www.ykls.app
+//2019年09月18日-23:41
+//CabinIcarus.IcSkillSystem.Runtime
+
+using CabinIcarus.IcSkillSystem.Runtime.Buffs.Entitys;
+using CabinIcarus.IcSkillSystem.Runtime.Skills.Components;
+
+namespace CabinIcarus.SkillSystem.Runtime.Skills.Com
+{
+    public interface ISkillFilter
+    {
+        /// <summary>
+        /// 过滤技能
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        bool Filter(IEntity entity,ISkillDataComponent skill);
+    }
+}
