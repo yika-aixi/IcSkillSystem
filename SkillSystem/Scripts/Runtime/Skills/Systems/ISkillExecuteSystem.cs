1faadeb34c60abe857bfddb9ea137aa8ba5b0f71
Icarus <1375400884@qq.com>
2019-09-18 19:20:42 +0800

    !T 新增了skill参数

--- SkillSystem/Scripts/Runtime/Skills/Systems/ISkillExecuteSystem.cs
+++ SkillSystem/Scripts/Runtime/Skills/Systems/ISkillExecuteSystem.cs
@@ -1,10 +1,11 @@
 ﻿using CabinIcarus.IcSkillSystem.Runtime.Buffs.Entitys;
+using CabinIcarus.IcSkillSystem.Runtime.Skills.Components;
 using CabinIcarus.IcSkillSystem.Runtime.Systems;
 
 namespace CabinIcarus.IcSkillSystem.Runtime.Skills.Systems
 {
     public interface ISkillExecuteSystem:ISkillSystem
     {
-        void Execute(IEntity entity);
+        void Execute(IEntity entity,ISkillDataComponent skill);
     }
 }
