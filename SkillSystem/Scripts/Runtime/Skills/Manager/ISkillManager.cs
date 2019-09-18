2b147a134120e7619d3ecbf81968c3097dc0dcba
Icarus <1375400884@qq.com>
2019-09-18 19:20:16 +0800

    !T 修改函数名

--- SkillSystem/Scripts/Runtime/Skills/Manager/ISkillManager.cs
+++ SkillSystem/Scripts/Runtime/Skills/Manager/ISkillManager.cs
@@ -1,12 +1,15 @@
 ﻿using CabinIcarus.IcSkillSystem.Runtime.Buffs.Entitys;
 using CabinIcarus.IcSkillSystem.Runtime.Skills.Components;
+using CabinIcarus.IcSkillSystem.Runtime.Skills.Systems;
 
 namespace CabinIcarus.IcSkillSystem.Runtime.Skills.Manager
 {
     public interface ISkillManager
     {
-        void AddSkill(IEntity entity, ISkillDataComponent skill);
+        void UseSkill(IEntity entity, ISkillDataComponent skill);
 
-        void RemoveSkill(IEntity entity, ISkillDataComponent skill);
+        //void RemoveSkill(IEntity entity, ISkillDataComponent skill);
+
+        void AddSkillSystme(ISkillExecuteSystem skillSystem);
     }
 }
