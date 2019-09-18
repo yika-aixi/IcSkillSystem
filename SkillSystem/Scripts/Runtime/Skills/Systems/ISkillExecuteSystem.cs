52bb7a45185224a89bf60cfba1a4f2b81299d664
Icarus <1375400884@qq.com>
2019-09-19 00:13:58 +0800

    !W 加上过滤

--- SkillSystem/Scripts/Runtime/Skills/Systems/ISkillExecuteSystem.cs
+++ SkillSystem/Scripts/Runtime/Skills/Systems/ISkillExecuteSystem.cs
@@ -1,10 +1,11 @@
 ﻿using CabinIcarus.IcSkillSystem.Runtime.Buffs.Entitys;
 using CabinIcarus.IcSkillSystem.Runtime.Skills.Components;
 using CabinIcarus.IcSkillSystem.Runtime.Systems;
+using CabinIcarus.SkillSystem.Runtime.Skills.Com;
 
 namespace CabinIcarus.IcSkillSystem.Runtime.Skills.Systems
 {
-    public interface ISkillExecuteSystem:ISkillSystem
+    public interface ISkillExecuteSystem:ISkillSystem,ISkillFilter
     {
         void Execute(IEntity entity,ISkillDataComponent skill);
     }
