23aa41b1d2c633e12595aff1c1ff70a045c25536
Icarus <1375400884@qq.com>
2019-09-19 20:05:51 +0800

    !A NPBehaveNode没有NPBehaveNode连接时设置为错误色(红色)

---
+++ xNode_NPBehave_Node/Editor/NPBehaveNodeEditor.cs
@@ -0,0 +1,44 @@
+﻿using CabinIcarus.IcSkillSystem.Runtime.xNode_NPBehave_Node;
+using UnityEditor;
+using UnityEngine;
+using XNodeEditor;
+using EditorGUIUtility = UnityEditor.Experimental.Networking.PlayerConnection.EditorGUIUtility;
+
+namespace CabinIcarus.IcSkillSystem.Editor.xNode_NPBehave_Node
+{
+    [NodeEditor.CustomNodeEditorAttribute(typeof(NPBehaveNode))]
+    public class NPBehaveNodeEditor:NodeEditor 
+    {
+        private NPBehaveNode _node;
+        private Color _backColor;
+        public void _check()
+        {
+            if (_node == null) _node = target as NPBehaveNode;
+            _backColor = GUI.color;
+        }
+
+        public override Color GetTint()
+        {
+            _check();
+
+            if (_node.Inputs != null && _node.Node == null)
+            {
+                return Color.red;
+            }
+            
+            return base.GetTint();
+        }
+
+        public override void OnBodyGUI()
+        {
+            _check();
+
+            if (_node.Inputs != null && _node.Node == null)
+            {
+                GUI.tooltip = "缺失连接点";
+            }
+            
+            base.OnBodyGUI();
+        }
+    }
+}
