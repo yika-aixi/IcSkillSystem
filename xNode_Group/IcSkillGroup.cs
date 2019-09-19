43c25cd9f487e9e41542091d22a37b81f95efae3
Icarus <1375400884@qq.com>
2019-09-19 22:39:25 +0800

    !A 技能图

---
+++ xNode_Group/IcSkillGroup.cs
@@ -0,0 +1,17 @@
+//创建者:Icarus
+//手动滑稽,滑稽脸
+//ヾ(•ω•`)o
+//https://www.ykls.app
+//2019年09月19日-21:25
+//Assembly-CSharp
+
+using UnityEngine;
+using XNode;
+
+namespace CabinIcarus.IcSkillSystem.xNode_Group
+{
+    [CreateAssetMenu(fileName = "New IcSkill Group",menuName = "CabinIcarus/IcSkillSystem/Group")]
+    public class IcSkillGroup:NodeGraph
+    {
+    }
+}
