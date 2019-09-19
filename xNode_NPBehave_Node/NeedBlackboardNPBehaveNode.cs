8df431fe297b60c54812988b6921470d8b4206b8
Icarus <1375400884@qq.com>
2019-09-20 00:40:27 +0800

    !A 需要黑板的行为节点

---
+++ xNode_NPBehave_Node/NeedBlackboardNPBehaveNode.cs
@@ -0,0 +1,28 @@
+//创建者:Icarus
+//手动滑稽,滑稽脸
+//ヾ(•ω•`)o
+//https://www.ykls.app
+//2019年09月19日-23:55
+//Assembly-CSharp
+
+using NPBehave;
+using UnityEngine;
+using XNode;
+
+namespace CabinIcarus.IcSkillSystem.Runtime.xNode_NPBehave_Node
+{
+    public abstract class NeedBlackboardNPBehaveNode:NPBehaveNode
+    {
+        [SerializeField,Input(ShowBackingValue.Never,ConnectionType.Override)]
+        private BlackboardNode _blackboardNode;
+
+        protected Blackboard Blackboard { get; private set; }
+
+        public override object GetValue(NodePort port)
+        {
+            Blackboard = GetInputValue(nameof(_blackboardNode), _blackboardNode)?.Blackboard;
+           
+            return base.GetValue(port);
+        }
+    }
+}
