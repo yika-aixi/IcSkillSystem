49a09e77db877a6797c7960a34e4a45fa4f3f637
Icarus <1375400884@qq.com>
2019-09-20 16:48:46 +0800

    !A ActionNode

---
+++ xNode_NPBehave_Node/Editor/ActionNodeEditor.cs
@@ -0,0 +1,20 @@
+﻿using CabinIcarus.IcSkillSystem.Runtime.xNode_NPBehave_Node.Tasks;
+using UnityEngine;
+using XNodeEditor;
+
+namespace CabinIcarus.IcSkillSystem.Editor.xNode_NPBehave_Node
+{
+    [NodeEditor.CustomNodeEditorAttribute(typeof(ActionNode))]
+    public class ActionNodeEditor:NodeEditor
+    {
+        public override Color GetTint()
+        {
+            if (!target.GetInputPort("_nodes").IsConnected)
+            {
+                return Color.red;
+            }
+            
+            return base.GetTint();
+        }
+    }
+}
---
+++ xNode_NPBehave_Node/Nodes/Task/ActionNode.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using CabinIcarus.IcSkillSystem.Runtime.xNode_NPBehave_Node.Attributes;
+using CabinIcarus.IcSkillSystem.Runtime.xNode_NPBehave_Node.Com;
+using NPBehave;
+using UnityEngine;
+using XNode;
+using Action = NPBehave.Action;
+
+namespace CabinIcarus.IcSkillSystem.Runtime.xNode_NPBehave_Node.Tasks
+{
+    [CreateNodeMenu("CabinIcarus/IcSkillSystem/Behave Nodes/Task/Action")]
+    public class ActionNode:NPBehaveNode
+    {
+        [SerializeField,Input(ShowBackingValue.Never,ConnectionType.Override)]
+        [PortTooltip("动作节点 - IActionNode")]
+        private NPBehaveNode _nodes;
+
+        [SerializeField,Output]
+        [PortTooltip("Action Node")]
+        private ActionNode _actionNode;
+
+        public override object GetValue(NodePort port)
+        {
+            _actionNode = (ActionNode) base.GetValue(port);
+
+            return _actionNode;
+        }
+
+        protected override void CreateNode()
+        {
+            switch (_nodes)
+            {
+                case IActionExecuteNode action:
+                    Node = new Action(action.Execute);      
+                    return;
+                case ISingleFrameFuncExecuteNode action:
+                    Node = new Action(action.Execute);      
+                    return;
+                case IMultiFrameFuncExecuteNode action:
+                    Node = new Action(action.Execute);      
+                    return;
+                case IMultiFrameFunc2ExecuteNode action:
+                    Node = new Action(action.Execute);      
+                    return;
+            }
+        }
+        
+        public override void OnCreateConnection(NodePort @from, NodePort to)
+        {
+            if (from.node != this)
+            {
+                if (!(from.node is IActionNode))
+                {
+                    from.Disconnect(to);
+                    Node = null;
+                    return;
+                }    
+            }
+            
+            base.OnCreateConnection(@from, to);
+        }
+
+        public override void OnRemoveConnection(NodePort port)
+        {
+            //输入断开,制空
+            if (port.IsInput)
+            {
+                Node = null;
+            }
+        }
+    }
+}
