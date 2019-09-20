343c1693e0d86f52a70df67184bc04f95355f75e
Icarus <1375400884@qq.com>
2019-09-21 03:14:26 +0800

    !B 逻辑多次跑问题,最开始想的时正确的=-=

--- xNode_Group/IcSkillGroup.cs
+++ xNode_Group/IcSkillGroup.cs
@@ -5,6 +5,8 @@
 //2019年09月19日-21:25
 //Assembly-CSharp
 
+using CabinIcarus.IcSkillSystem.Runtime.xNode_NPBehave_Node;
+using NPBehave;
 using UnityEngine;
 using XNode;
 
@@ -13,5 +15,28 @@ namespace CabinIcarus.IcSkillSystem.xNode_Group
     [CreateAssetMenu(fileName = "New IcSkill Group",menuName = "CabinIcarus/IcSkillSystem/Group")]
     public class IcSkillGroup:NodeGraph
     {
+        /// <summary>
+        /// 开始,返回Node
+        /// </summary>
+        /// <returns></returns>
+        public Root Start()
+        {
+            RootNode rootNode = null;
+            foreach (var node in nodes)
+            {
+                if (node.GetType() == typeof(RootNode))
+                {
+                    rootNode = (RootNode) node.GetValue(null);
+                    break;
+                }
+            }
+
+            if (!rootNode)
+            {
+                return null;
+            }
+
+            return (Root) rootNode.Node;
+        }
     }
 }
