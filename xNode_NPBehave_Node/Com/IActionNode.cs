3f3256bf195d06e0bfa34907c251e067a56f3197
Icarus <1375400884@qq.com>
2019-09-20 16:48:23 +0800

    !A Action动作接口

---
+++ xNode_NPBehave_Node/Com/IActionNode.cs
@@ -0,0 +1,28 @@
+﻿using NPBehave;
+
+namespace CabinIcarus.IcSkillSystem.Runtime.xNode_NPBehave_Node.Com
+{
+    public interface IActionNode
+    {
+    }
+
+    public interface IActionExecuteNode:IActionNode
+    {
+        void Execute();
+    }
+    
+    public interface ISingleFrameFuncExecuteNode:IActionNode
+    {
+        bool Execute();
+    }
+    
+    public interface IMultiFrameFuncExecuteNode:IActionNode
+    {
+        Action.Result Execute(bool arg);
+    }
+    
+    public interface IMultiFrameFunc2ExecuteNode:IActionNode
+    {
+        Action.Result Execute(Action.Request request);
+    }
+}
