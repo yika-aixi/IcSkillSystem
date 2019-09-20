5e141f4f4c1ee2f2ae9d5b946e21371705cc15db
Icarus <1375400884@qq.com>
2019-09-20 16:49:38 +0800

    !A 技能条件节点

---
+++ xNode_NPBehave_Node/Editor/SkillConditionNodeEditor.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using CabinIcarus.IcSkillSystem.Runtime.Skills.Condition;
+using CabinIcarus.IcSkillSystem.Runtime.xNode_NPBehave_Node.SkillSystems;
+using UnityEditor;
+using UnityEngine;
+using XNodeEditor;
+
+namespace CabinIcarus.IcSkillSystem.Editor.xNode_NPBehave_Node
+{
+    [NodeEditor.CustomNodeEditorAttribute(typeof(SkillConditionNode))]
+    public class SkillConditionNodeEditor:AQNameSelectEditor<SkillConditionNode>
+    {
+        private SerializedProperty _blackboardProperty;
+        public override void OnCreate()
+        {
+            _blackboardProperty = serializedObject.FindProperty("_blackboardNode");
+            
+            base.OnCreate();
+        }
+
+        protected override Type GetBaseType()
+        {
+            return typeof(ACondition);
+        }
+
+        public override void OnBodyGUI()
+        {
+            _baseDraw();
+            
+            DrawSelectPop(new GUIContent("Skill Condition"));
+        }
+
+        protected override string GetAQNamePropertyName()
+        {
+            return "_conditionAQName";
+        }
+        
+        private void _baseDraw()
+        {
+            NodeEditorGUILayout.PropertyField(_blackboardProperty);
+            NodeEditorGUILayout.PropertyField(serializedObject.FindProperty("_conditionNode"));
+        }
+    }
+}
---
+++ xNode_NPBehave_Node/Nodes/SkillSystem/SkillConditionNode.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using CabinIcarus.IcSkillSystem.Runtime.Buffs;
+using CabinIcarus.IcSkillSystem.Runtime.Buffs.Entitys;
+using CabinIcarus.IcSkillSystem.Runtime.Skills.Condition;
+using CabinIcarus.IcSkillSystem.Runtime.xNode_NPBehave_Node.Com;
+using UnityEngine;
+using XNode;
+
+namespace CabinIcarus.IcSkillSystem.Runtime.xNode_NPBehave_Node.SkillSystems
+{
+    [CreateNodeMenu("CabinIcarus/IcSkillSystem/Skill/Condition")]
+    public class SkillConditionNode:NeedBlackboardNPBehaveNode,ISingleFrameFuncExecuteNode
+    {
+        [SerializeField]
+        private string _conditionAQName;
+        
+        [SerializeField,Output()]
+        private SkillConditionNode _conditionNode;
+
+        private ACondition _condition;
+
+        public override object GetValue(NodePort port)
+        {
+            _conditionNode = (SkillConditionNode) base.GetValue(port);
+            return _conditionNode;
+        }
+
+        protected override void CreateNode()
+        {
+            if (Blackboard == null)
+            {
+                return;
+            }
+            
+            var buffManager = Blackboard.Get<IBuffManager>(BlackBoardConstKeyTables.BuffManager);
+
+            var connditionType = Type.GetType(_conditionAQName);
+            
+            _condition = (ACondition) Activator.CreateInstance(connditionType,buffManager);
+        }
+
+        public bool Execute()
+        {
+            if (_condition == null)
+            {
+                return false;
+            }
+            
+            var entity = Blackboard.Get<IEntity>(BlackBoardConstKeyTables.UseSkillEntity);
+
+            return _condition.Check(entity);
+        }
+    }
+}
