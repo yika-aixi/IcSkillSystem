3c1d2f1ab7cf89e170ee85f737cd6eecfb7f8a0d
Icarus <1375400884@qq.com>
2019-09-21 23:14:44 +0800

    !O 因抽出抽象类而发生的改变

--- xNode_NPBehave_Node/Nodes/Composite/SequenceNode.cs
+++ xNode_NPBehave_Node/Nodes/Composite/SequenceNode.cs
@@ -1,54 +1,15 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
-using CabinIcarus.IcSkillSystem.Runtime.xNode_NPBehave_Node.Attributes;
 using NPBehave;
-using UnityEngine;
-using XNode;
 using Node = NPBehave.Node;
 
 namespace CabinIcarus.IcSkillSystem.Runtime.xNode_NPBehave_Node.Composite
 {
 	[XNode.Node.CreateNodeMenu("CabinIcarus/IcSkillSystem/Behave Nodes/Composite/Sequence")]
-	public class SequenceNode : NPBehaveNode
+	public class SequenceNode : ACompositeNode
 	{
-		[SerializeField,Input(typeConstraint = TypeConstraint.Inherited)]
-		[PortTooltip("节点,可多个")]
-		private NPBehaveNode _nodes;
-
-		protected override void CreateNode()
+		protected override Node GetNode(List<Node> inputNodes)
 		{
-			var nodes = GetInputValue(nameof(_nodes), _nodes);
-
-			if (nodes != null)
-			{
-				List<Node> inputNodes = new List<Node>();
-				foreach (var nodePort in Inputs)
-				{
-					foreach (var connection in nodePort.GetConnections())
-					{
-						var node = (NPBehaveNode) connection.node;
-
-						if (node.Node == null)
-						{
-							continue;
-						}
-
-						inputNodes.Add(node.Node);
-					}
-				}
-
-				if (inputNodes.Count > 0)
-				{
-					Node = new Sequence(inputNodes.ToArray());
-				}
-			}
-			else
-			{
-				if (Node != null)
-				{
-					Node = null;
-				}
-			}
+			return new Sequence(inputNodes.ToArray());
 		}
 	}
 }
