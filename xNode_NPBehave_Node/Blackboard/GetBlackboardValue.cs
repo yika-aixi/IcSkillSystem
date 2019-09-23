b79a405c81ba369f2da2b38ce464a5236fce9bb7
Icarus <1375400884@qq.com>
2019-09-23 17:24:51 +0800

    !A 获取黑板值得值节点

---
+++ xNode_NPBehave_Node/Blackboard/GetBlackboardValue.cs
@@ -0,0 +1,41 @@
+﻿using CabinIcarus.IcSkillSystem.Runtime.xNode_Nodes;
+using CabinIcarus.IcSkillSystem.Runtime.xNode_NPBehave_Node.Attributes;
+using NPBehave;
+using UnityEngine;
+using Node = XNode.Node;
+
+namespace CabinIcarus.IcSkillSystem.Runtime.xNode_NPBehave_Node
+{
+    [Node.CreateNodeMenuAttribute("CabinIcarus/IcSkillSystem/Behave Nodes/Blackboard/Get Blackboard Value")]
+    public class GetBlackboardValue:ValueNode
+    {
+        [SerializeField,Input(ShowBackingValue.Never,ConnectionType.Override,TypeConstraint.Inherited)]
+        [PortTooltip("黑板 Node")]
+        private BlackboardNode _blackBoardNode;
+        
+        [SerializeField]
+        private string _key;
+        
+        public override object Value => _getValue();
+
+        private Blackboard _blackboard;
+
+        protected override void Init()
+        {
+            base.Init();
+
+#if UNITY_EDITOR
+            if (!Application.isPlaying)
+            {
+                return;
+            }
+#endif
+            _blackboard = GetInputValue(nameof(_blackBoardNode), _blackBoardNode).Blackboard;
+        }
+        
+        private object _getValue()
+        {
+            return _blackboard?.Get(_key);
+        }
+    }
+}
