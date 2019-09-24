7dfc16f4e8831f8ad65a5b3335905ff54189722a
Icarus <1375400884@qq.com>
2019-09-24 18:10:26 +0800

    !A Type 工具类

---
+++ xNode_NPBehave_Node/Editor/Utils/TypeUtil.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using DesperateDevs.Utils;
+
+namespace CabinIcarus.IcSkillSystem.Editor.xNode_NPBehave_Node.Utils
+{
+    public static class TypeUtil
+    {
+        private static List<Type> _objectTypes = new List<Type>();
+
+        public static IEnumerable<Type> AllTypes => _objectTypes;
+        
+        public static IEnumerable<Type> RuntimeTypes => _objectTypes.Where(x=>!x.Assembly.FullName.Contains("Editor"));
+
+        static TypeUtil()
+        {
+            _collectValueTyps();
+        }
+
+        private static void _collectValueTyps()
+        {
+            _objectTypes.AddRange(AppDomain.CurrentDomain.GetAllTypes().Where(x => x.IsPublic));
+        }
+    }
+}
