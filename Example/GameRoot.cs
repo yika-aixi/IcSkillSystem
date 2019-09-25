af7e8ecc627cc9519c043c8a8df518b497024734
Icarus <1375400884@qq.com>
2019-09-25 22:17:37 +0800

    !DT 删除了代码离播放音效的逻辑,转移到了行为树中配置,将时间处理系统拉到第一

--- Example/GameRoot.cs
+++ Example/GameRoot.cs
@@ -25,9 +25,6 @@ namespace CabinIcarus.IcSkillSystem.Expansions
 
         public string SkillManagerKey = "SkillManager";
 
-        public AudioClip Clip;
-        private AudioSource _source;
-
         private void Awake()
         {
             var bb = UnityContext.GetSharedBlackboard(SharedBlackboardKey);
@@ -38,12 +35,12 @@ namespace CabinIcarus.IcSkillSystem.Expansions
             bb.Set(SkillManagerKey,SkillManager);
             
             BuffManager
+                //带有持续时间buff的时间处理,一定在所有buff之前,因为时间的减少发生在buff产生效果之前
+                .AddBuffSystem(new BuffTimeSystem(BuffManager))
                 //固定减伤buff
                 .AddBuffSystem(new DamageReduceFixedSystem(BuffManager))
                 //百分比减伤buff
                 .AddBuffSystem(new DamageReducePercentageSystem(BuffManager))
-                //带有持续时间buff的时间处理,一定在所有持续类buff之前,因为时间的减少发生在buff产生效果之前
-                .AddBuffSystem(new BuffTimeSystem(BuffManager))
                 //持续伤害buff
                 .AddBuffSystem(new ContinuousDamageSystem<DamageBuff>(BuffManager))
                 //伤害buff
@@ -53,23 +50,13 @@ namespace CabinIcarus.IcSkillSystem.Expansions
                 .AddBuffSystem(new DamageReducePercentageShowSystem(StateManager))
                 .AddBuffSystem(new DamageReduceFixedBuffIconShowSystem(StateManager));
             
-            
             SkillManager
                 // Hello World Skill 同时随机一个buff
                 .AddSkillSystem(new Skill1System(BuffManager))
                 .AddSkillSystem(new Skill2System(BuffManager))
                 .AddSkillSystem(new Skill3System(BuffManager))
                 .AddSkillSystem(new Skill4System(BuffManager))
-                .AddSkillSystem(new Skill4AudioSystem(_playClip));
-
-            _source = gameObject.AddComponent<AudioSource>();
-            _source.playOnAwake = false;
-            _source.clip = Clip;
-        }
-
-        private void _playClip()
-        {
-            _source.Play();
+                .AddSkillSystem(new PlayAudioSystem());
         }
 
         private void Update()
