2d04d1217ac914cbaa5475d2ea514e67fc69809c
Icarus <1375400884@qq.com>
2019-09-25 13:11:30 +0800

    !A ValueNodeEditor

---
+++ xNode_Nodes/Editor/ValueNodeEditor.cs
@@ -0,0 +1,36 @@
+﻿using CabinIcarus.IcSkillSystem.Runtime.xNode_Nodes;
+using XNode;
+using XNodeEditor;
+
+namespace UnityEditor
+{
+    [NodeEditor.CustomNodeEditorAttribute(typeof(ValueNode))]
+    public class ValueNodeEditor:NodeEditor
+    {
+        private ValueNode _valueNode;
+        public override void OnCreate()
+        {
+            base.OnCreate();
+
+            _valueNode = (ValueNode) target;
+        }
+
+        public override void OnBodyGUI()
+        {
+            var port = target.GetOutputPort(ValueNode.ValueOutPutPortName);
+
+            if (port == null)
+            {
+                target.AddDynamicOutput(_valueNode.ValueType, fieldName: ValueNode.ValueOutPutPortName);
+                return;
+            }
+
+            if (port.ValueType != _valueNode.ValueType)
+            {
+                port.ValueType = _valueNode.ValueType;
+            }
+            
+            base.OnBodyGUI();
+        }
+    }
+}
