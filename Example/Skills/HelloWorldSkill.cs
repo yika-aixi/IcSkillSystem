a6cbb9946ee51ffbd9e490853153fff00549390c
Icarus <1375400884@qq.com>
2019-09-26 00:13:16 +0800

    !D 没用资源,不小心提交=-=

--- Example/Skills/HelloWorldSkill.cs
+++
@@ -1,9 +0,0 @@
-﻿using CabinIcarus.IcSkillSystem.Runtime.Skills.Components;
-
-namespace CabinIcarus.IcSkillSystem.Expansions.Skills
-{
-    public class HelloWorldSkill:ISkillDataComponent
-    {
-        public float NeedMona;
-    }
-}
