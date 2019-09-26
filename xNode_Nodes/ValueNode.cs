8d51fa4b34bb183ff06ab4ad19d864c831a98b91
Icarus <1375400884@qq.com>
2019-09-26 17:25:04 +0800

    !T

--- xNode_Nodes/ValueNode.cs
+++ xNode_Nodes/ValueNode.cs
@@ -7,7 +7,47 @@ using XNode;
 namespace CabinIcarus.IcSkillSystem.Runtime.xNode_Nodes
 {
     /// <summary>
-    /// 值节点,为了严格性,请在实现类在output一个实际类型,如IntNode
+    /// 值节点
+    /// </summary>
+    public abstract class ValueNode:Node,IIcSkillSystemNode
+    {
+        public const string ValueOutPutPortName = "Value";
+
+        public override object GetValue(NodePort port)
+        {
+            if (port.fieldName == ValueOutPutPortName)
+            {
+                return GetOutValue();
+            }
+            
+            return this;
+        }
+
+        /// <summary>
+        /// Value类型
+        /// </summary>
+        public abstract Type ValueType { get;}
+
+
+        /// <summary>
+        /// 是否能改变Value类型
+        /// </summary>
+        public virtual bool IsChangeValueType { get; } = false;
+
+        /// <summary>
+        /// 当是可改变类型是,只会显示该类型可以分配了的类型,默认为system.object
+        /// </summary>
+        public virtual Type BaseType { get; } = typeof(object);
+        
+        /// <summary>
+        /// 获取输出 value
+        /// </summary>
+        /// <returns></returns>
+        protected abstract object GetOutValue();
+    }
+    
+    /// <summary>
+    /// 值节点的条件节点
     /// </summary>
     public abstract class ValueNode:Node,IIcSkillSystemNode
     {
