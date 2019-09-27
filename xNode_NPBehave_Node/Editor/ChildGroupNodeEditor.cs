42c700c159449be9f1fc7f1cfe7e7f5acddaef64
Icarus <1375400884@qq.com>
2019-09-27 18:02:03 +0800

    !A  子图节点

---
+++ xNode_NPBehave_Node/Editor/ChildGroupNodeEditor.cs
@@ -0,0 +1,48 @@
+﻿using CabinIcarus.IcSkillSystem.Runtime.xNode_NPBehave_Node;
+using NPBehave;
+using UnityEditor;
+using UnityEngine;
+using XNodeEditor;
+
+namespace CabinIcarus.IcSkillSystem.Editor.xNode_NPBehave_Node
+{
+    [NodeEditor.CustomNodeEditorAttribute(typeof(ChildGroupNode))]
+    public class ChildGroupNodeEditor:NodeEditor 
+    {
+        private ChildGroupNode rootNode;
+
+        public override Color GetTint()
+        {
+            _check();
+            
+            if (!rootNode.GetPort("_main").IsConnected)
+            {
+                return new Color(205 / 255f,20 / 255f,25 / 255f);
+            }            
+            
+            return new Color(30 / 255f,147 / 255f,65 / 255f);
+        }
+
+        public override void OnBodyGUI()
+        {
+            serializedObject.Update();
+            
+            EditorGUILayout.BeginHorizontal();
+            {
+                {
+                    EditorGUILayout.LabelField(string.Empty,GUILayout.Width(GetWidth() / 2));
+                    NodeEditorGUILayout.PortField(new GUIContent("Main Node"),target.GetPort("_main"));
+                }
+            }
+            EditorGUILayout.EndHorizontal();
+
+            // Apply property modifications
+            serializedObject.ApplyModifiedProperties();
+        }
+
+        private void _check()
+        {
+            if (rootNode == null) rootNode = target as ChildGroupNode;
+        }
+    }
+}
