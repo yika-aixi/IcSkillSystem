380ea576bf100aa9a4043d8c6db56037d05a0506
Icarus <1375400884@qq.com>
2019-09-27 16:11:29 +0800

    !WF 构造函数的调用逻辑

--- xNode_NPBehave_Node/Utils/xNodeExpansion.cs
+++ xNode_NPBehave_Node/Utils/xNodeExpansion.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using XNode;
 
@@ -6,21 +8,51 @@ namespace SkillSystem.xNode_NPBehave_Node.Utils
 {
     public static class xNodeExpansion
     {
+        private const string _ctorArgName = "(ctor arg)";
+
+        public static string GetCtorParameterName(string argName)
+        {
+            return $"{argName}{_ctorArgName}";
+        }
+
+        public static bool IsCtorParameter(string name)
+        {
+            return name.Contains(_ctorArgName);
+        }
+
         /// <summary>
         /// 动态输入创建类型实例
         /// </summary>
         /// <param name="self"></param>
         /// <param name="instanceType"></param>
+        /// <param name="args">如果是空或者个数为0时将会自动寻找第一个默认构造函数进行设置</param>
         /// <returns></returns>
         public static object DynamicInputCreateInstance(this Node self, Type instanceType,params object[] args)
         {
             //todo 断言保护
-            //
+            
+            if (args == null || args.Length == 0)
+            {
+                var defaultCtor = instanceType.GetConstructors()[0];
+                List<object> cArg = new List<object>();
+                foreach (var parameterInfo in defaultCtor.GetParameters())
+                {
+                     var ctorInput = self.GetInputPort(GetCtorParameterName(parameterInfo.Name));
+                     cArg.Add(ctorInput.GetInputValue());
+                }
+
+                args = cArg.ToArray();
+            }
             
             var ins = Activator.CreateInstance(instanceType,args);
 
             foreach (var dynamicInput in self.DynamicInputs)
             {
+                if (IsCtorParameter(dynamicInput.fieldName))
+                {
+                    continue;
+                }
+                
                 var value = dynamicInput.GetInputValue();
 
                 if (value == null)
