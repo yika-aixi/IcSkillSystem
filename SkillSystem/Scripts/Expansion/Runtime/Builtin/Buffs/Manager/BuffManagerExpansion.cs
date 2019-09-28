e4ec04761277b8c6f100059e9c5089c8e37e099a
Icarus <1375400884@qq.com>
2019-09-29 02:01:31 +0800

    !A 基于引用池的buff扩展(有点问题出现,后面排查)

---
+++ SkillSystem/Scripts/Expansion/Runtime/Builtin/Buffs/Manager/BuffManagerExpansion.cs
@@ -0,0 +1,77 @@
+//创建者:Icarus
+//手动滑稽,滑稽脸
+//ヾ(•ω•`)o
+//https://www.ykls.app
+//2019年09月28日-20:42
+//CabinIcarus.IcSkillSystem.Expansion.Runtime
+
+using System;
+using CabinIcarus.IcSkillSystem.Runtime.Buffs;
+using CabinIcarus.IcSkillSystem.Runtime.Buffs.Components;
+using CabinIcarus.IcSkillSystem.Runtime.Buffs.Entitys;
+using IcSkillSystem.SkillSystem.Scripts.Expansion.Runtime.Builtin.Utils;
+using UnityEngine;
+
+namespace CabinIcarus.IcSkillSystem.Expansion.Runtime.Builtin.Buffs
+{
+    public static class BuffManagerExpansion
+    {
+        private static ReferenceObjectPool _buffPool;
+        
+        static BuffManagerExpansion()
+        {
+            _buffPool = new ReferenceObjectPool();
+            _buffPool.RecedeCache = true;
+        }
+
+        public static T CreateBuff<T>(this IBuffManager self, params object[] args) where T : IBuffDataComponent
+        {
+            return (T) self.CreateBuff(typeof(T), args);
+        }
+        
+        public static IBuffDataComponent CreateBuff(this IBuffManager self,Type buffType, params object[] args)
+        {
+            if (!typeof(IBuffDataComponent).IsAssignableFrom(buffType))
+            {
+                return null;
+            }
+            
+            var buff = (IBuffDataComponent) _buffPool.GetObject(buffType,args);
+
+            return buff;
+        }
+
+        public static void CreateAndAddBuff<T>(this IBuffManager self,IEntity entity, Action<T> buffSetting,params object[] args) where T : IBuffDataComponent
+        {
+            if (buffSetting == null)
+            {
+                self.CreateAndAddBuff(typeof(T), entity, null, args);
+            }
+            else
+            {
+                self.CreateAndAddBuff(typeof(T), entity, x=>buffSetting.Invoke((T) x), args);
+            }
+        }
+        
+        public static void CreateAndAddBuff(this IBuffManager self,Type buffType,IEntity entity, Action<IBuffDataComponent> buffSetting,params object[] args)
+        {
+            IBuffDataComponent buff = self.CreateBuff(buffType, args);
+            
+            if (buff == null)
+            {
+                return;
+            }
+            
+            buffSetting?.Invoke(buff);
+            
+            self.AddBuff(entity,buff);
+        }
+        
+        public static void RemoveBuffEx(this IBuffManager self,IEntity entity,IBuffDataComponent buff)
+        {
+            self.RemoveBuff(entity, buff);
+            
+            _buffPool.Recede(buff);
+        }
+    }
+}
