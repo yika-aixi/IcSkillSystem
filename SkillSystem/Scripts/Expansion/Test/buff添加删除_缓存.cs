5ab4c6de87bc0ca018ede56ec7a2110739595a6f
Icarus <1375400884@qq.com>
2019-09-29 19:22:36 +0800

    !A(BuffManager) 测试代码

---
+++ SkillSystem/Scripts/Expansion/Test/buff添加删除_缓存.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using CabinIcarus.IcSkillSystem.Expansion.Runtime.Builtin.Buffs;
+using CabinIcarus.IcSkillSystem.Expansion.Runtime.Builtin.Buffs.Unity;
+using CabinIcarus.IcSkillSystem.Runtime.Buffs.Components;
+using UnityEngine;
+
+namespace IcSkillSystem.SkillSystem.Expansion.Tests
+{
+    public class buff添加删除_缓存 : MonoBehaviour
+    {
+        private BuffManager _buffManage;
+        private Enity _entity;
+        
+        private void Start()
+        {
+            _buffManage = new BuffManager();
+            _entity = new Enity();
+            
+            _buffAddAndRemove();
+
+            GameObject go = new GameObject();
+
+            var buffL = go.AddComponent<BuffEntityLinkComponent>();
+        
+            buffL.Init(_buffManage,_entity);
+        
+        }
+
+        [ContextMenu("Add")]
+        private void _buffAdd()
+        {
+            var type = typeof(TestBuff);
+
+            for (int i = 0; i < 5000; i++)
+            {
+                var i1 = i;
+                var buff = _buffManage.CreateAndAddBuff(type, _entity, x => { ((TestBuff) x).name = i1.ToString(); });
+            }
+        }
+        
+        [ContextMenu("Add And Remove")]
+        private void _buffAddAndRemove()
+        {
+            var type = typeof(TestBuff);
+
+            for (int i = 0; i < 5000; i++)
+            {
+                var i1 = i;
+                var buff = _buffManage.CreateAndAddBuff(type, _entity, x => { ((TestBuff) x).name = i1.ToString(); });
+                _buffManage.RemoveBuffEx(_entity, buff);
+            }
+        }
+    }
+}
