fa2fa33687105797c2f5d3aeb79845a3d79a1ea1
Icarus <1375400884@qq.com>
2019-10-08 14:54:34 +0800

    !A 类型错误异常

---
+++ SkillSystem/Scripts/Expansion/Runtime/Builtin/Buffs/Exceptions/TypeErrorException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace IcSkillSystem.SkillSystem.Scripts.Expansion.Runtime.Builtin.Buffs.Exceptions
+{
+    public class TypeErrorException:Exception
+    {
+        public Type TargetType { get; }
+
+        public TypeErrorException(Type targetType)
+        {
+            TargetType = targetType;
+        }
+
+        public TypeErrorException(string message, Type targetType) : base(message)
+        {
+            TargetType = targetType;
+        }
+
+        public TypeErrorException(string message, Exception innerException, Type targetType) : base(message, innerException)
+        {
+            TargetType = targetType;
+        }
+    }
+}
