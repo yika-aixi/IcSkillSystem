e40609f92da7236acd9ea2abce60ecc13e776569
Icarus <1375400884@qq.com>
2019-10-09 16:55:35 +0800

    !A 先提交下,底层将进行重写,之前的写法无法解决装箱/拆箱,接口约定导致

---
+++ SkillSystem/Scripts/Runtime/Buffs/Manager/INewBuffManager.cs
@@ -0,0 +1,16 @@
+﻿using CabinIcarus.IcSkillSystem.Runtime.Buffs.Systems.Interfaces;
+
+namespace CabinIcarus.IcSkillSystem.Runtime.Buffs
+{
+    public interface INewBuffManager
+    {
+        /// <summary>
+        /// 添加buff System
+        /// </summary>
+        /// <param name="buffSystem"></param>
+        /// <returns></returns>
+        INewBuffManager AddBuffSystem(IBuffSystem buffSystem);
+        
+        
+    }
+}
---
+++ SkillSystem/Scripts/Runtime/Buffs/Systems/AIcBuffSystem.cs
@@ -0,0 +1,19 @@
+﻿using CabinIcarus.IcSkillSystem.Runtime.Buffs.Systems.Interfaces;
+
+namespace CabinIcarus.IcSkillSystem.Runtime.Buffs.Systems
+{
+    public abstract class AIcBuffSystem : IBuffCreateSystem,IBuffUpdateSystem,IBuffDestroySystem
+    {
+//        protected readonly IBuffManager<T> BuffManager;
+//
+//        protected AIcBuffSystem(IBuffManager<T> buffManager)
+//        {
+//            BuffManager = buffManager;
+//        }
+        
+        public abstract bool Filter();
+        public abstract void Destroy();
+        public abstract void Execute();
+        public abstract void Create();
+    }
+}
