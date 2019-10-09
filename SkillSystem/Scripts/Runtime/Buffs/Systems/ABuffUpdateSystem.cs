4874ce8d0c6c7efe52ef90933cc19a7a7cc31368
Icarus <1375400884@qq.com>
2019-10-09 16:56:40 +0800

    !T 注释代码

--- SkillSystem/Scripts/Runtime/Buffs/Systems/ABuffUpdateSystem.cs
+++ SkillSystem/Scripts/Runtime/Buffs/Systems/ABuffUpdateSystem.cs
@@ -1,26 +1,26 @@
-//创建者:Icarus
-//手动滑稽,滑稽脸
-//ヾ(•ω•`)o
-//https://www.ykls.app
-//2019年09月14日-19:30
-//CabinIcarus.SkillSystem.Runtime
-
-using CabinIcarus.IcSkillSystem.Runtime.Buffs.Components;
-using CabinIcarus.IcSkillSystem.Runtime.Buffs.Entitys;
-using CabinIcarus.IcSkillSystem.Runtime.Buffs.Systems.Interfaces;
-
-namespace CabinIcarus.IcSkillSystem.Runtime.Buffs.Systems
-{
-    public abstract class ABuffUpdateSystem<T> : IBuffUpdateSystem where T : IBuffDataComponent
-    {
-        protected readonly IBuffManager<T> BuffManager;
-
-        protected ABuffUpdateSystem(IBuffManager<T> buffManager)
-        {
-            BuffManager = buffManager;
-        }
-        
-        public abstract bool Filter(IEntity entity);
-        public abstract void Execute(IEntity entity);
-    }
-}
+////创建者:Icarus
+////手动滑稽,滑稽脸
+////ヾ(•ω•`)o
+////https://www.ykls.app
+////2019年09月14日-19:30
+////CabinIcarus.SkillSystem.Runtime
+//
+//using CabinIcarus.IcSkillSystem.Runtime.Buffs.Components;
+//using CabinIcarus.IcSkillSystem.Runtime.Buffs.Entitys;
+//using CabinIcarus.IcSkillSystem.Runtime.Buffs.Systems.Interfaces;
+//
+//namespace CabinIcarus.IcSkillSystem.Runtime.Buffs.Systems
+//{
+//    public abstract class ABuffUpdateSystem<T> : IBuffUpdateSystem where T : IBuffDataComponent
+//    {
+//        protected readonly IBuffManager<T> BuffManager;
+//
+//        protected ABuffUpdateSystem(IBuffManager<T> buffManager)
+//        {
+//            BuffManager = buffManager;
+//        }
+//        
+//        public abstract bool Filter(IEntity entity);
+//        public abstract void Execute(IEntity entity);
+//    }
+//}
