49bdf6a7f15a3608b964f6ca734504f1ca033c60
Icarus <1375400884@qq.com>
2019-10-10 18:48:41 +0800

    !T

--- SkillSystem/Scripts/Runtime/Buffs/Manager/INewBuffManager.cs
+++ SkillSystem/Scripts/Runtime/Buffs/Manager/INewBuffManager.cs
@@ -1,4 +1,6 @@
-﻿using CabinIcarus.IcSkillSystem.Runtime.Buffs.Systems.Interfaces;
+﻿using CabinIcarus.IcSkillSystem.Runtime.Buffs.Components;
+using CabinIcarus.IcSkillSystem.Runtime.Buffs.Systems;
+using CabinIcarus.IcSkillSystem.Runtime.Buffs.Systems.Interfaces;
 
 namespace CabinIcarus.IcSkillSystem.Runtime.Buffs
 {
