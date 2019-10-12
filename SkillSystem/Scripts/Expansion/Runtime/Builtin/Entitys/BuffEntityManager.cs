a07bb105760c6715f1d4c1c3ecdd71907c24d07a
Icarus <1375400884@qq.com>
2019-10-12 13:06:26 +0800

    !A EntityManager

---
+++ SkillSystem/Scripts/Expansion/Runtime/Builtin/Entitys/BuffEntityManager.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using CabinIcarus.IcSkillSystem.Expansion.Runtime.Builtin.Buffs;
+using CabinIcarus.IcSkillSystem.Runtime.Buffs;
+using CabinIcarus.IcSkillSystem.Runtime.Buffs.Components;
+using CabinIcarus.IcSkillSystem.Runtime.Buffs.Entitys;
+using CabinIcarus.IcSkillSystem.Runtime.Buffs.Systems;
+using CabinIcarus.IcSkillSystem.SkillSystem.Runtime.Utils;
+
+namespace SkillSystem.SkillSystem.Scripts.Expansion.Runtime.Builtin.Entitys
+{
+    public class BuffEntityManager:IStructBuffEntityManager<NewBuffManager>
+    {
+        private FasterList<BuffEntity> _entitys;
+
+        
+        public NewBuffManager BuffManager { get; }
+        
+        public BuffEntityManager(NewBuffManager buffManager)
+        {
+            BuffManager = buffManager;
+            _entitys = new FasterList<BuffEntity>();
+        }
+
+        private int _id;
+        
+        public BuffEntity CreateEntity()
+        {
+            ++_id;
+
+            return CreateEntity(_id);
+        }
+
+        public BuffEntity CreateEntity(int id)
+        {
+            BuffEntity entity = id;
+            
+            BuffManager.AddEntity(entity);
+            
+            return entity;
+        }
+
+        public bool DestroyEntity(int id)
+        {
+            return DestroyEntity((BuffEntity) id);
+        }
+
+        public bool DestroyEntity(BuffEntity entity)
+        {
+            if (!_checkEntity(entity))
+            {
+                return false;
+            }
+            
+            BuffManager.RemoveEntity(entity);
+
+            return true;
+        }
+
+        private bool _checkEntity(BuffEntity entity)
+        {
+            return _entitys.Contains(entity);
+        }
+
+        public void Update()
+        {
+            BuffManager.Update();
+        }
+
+        public void AddBuff<T>(BuffEntity entity, T buff) where T :struct, IBuffDataComponent
+        {
+            BuffManager.AddBuff(entity,buff);
+        }
+
+        public bool RemoveBuff<T>(BuffEntity entity, T buff) where T :struct, IBuffDataComponent
+        {
+            return BuffManager.RemoveBuff(entity, buff);
+        }
+
+        public bool HasBuff<T>(BuffEntity entity, T buff) where T :struct, IBuffDataComponent
+        {
+            return BuffManager.HasBuff(entity, buff);
+        }
+
+        #region Cover
+
+        void IBuffEntityManager<NewBuffManager>.AddBuff<T>(BuffEntity entity, T buff)
+        {
+            throw new NotImplementedException($"Type is {nameof(IStructBuffEntityManager<NewBuffManager>)}");
+        }
+
+        bool IBuffEntityManager<NewBuffManager>.RemoveBuff<T>(BuffEntity entity, T buff)
+        {
+            throw new NotImplementedException($"Type is {nameof(IStructBuffEntityManager<NewBuffManager>)}");
+        }
+
+        bool IBuffEntityManager<NewBuffManager>.HasBuff<T>(BuffEntity entity, T buff)
+        {
+            throw new NotImplementedException($"Type is {nameof(IStructBuffEntityManager<NewBuffManager>)}");
+        }
+
+        #endregion
+    }
+}
---
+++ SkillSystem/Scripts/Runtime/Entitys/IBuffEntityManager.cs
@@ -0,0 +1,34 @@
+﻿using CabinIcarus.IcSkillSystem.Runtime.Buffs.Components;
+
+namespace CabinIcarus.IcSkillSystem.Runtime.Buffs.Entitys
+{
+    public interface IBuffEntityManager<TBM> where TBM : INewBuffManager
+    {
+        TBM BuffManager { get;}
+
+        BuffEntity CreateEntity();
+        
+        BuffEntity CreateEntity(int id);
+
+        bool DestroyEntity(int id);
+        
+        bool DestroyEntity(BuffEntity entity);
+
+        void Update();
+
+        void AddBuff<T>(BuffEntity entity, T buff) where T : IBuffDataComponent;
+        
+        bool RemoveBuff<T>(BuffEntity entity,T buff) where T : IBuffDataComponent;
+
+        bool HasBuff<T>(BuffEntity entity, T buff) where T : IBuffDataComponent;
+    }
+
+    public interface IStructBuffEntityManager<TBM> : IBuffEntityManager<TBM> where TBM : INewBuffManager
+    {
+        new void AddBuff<T>(BuffEntity entity, T buff) where T :struct, IBuffDataComponent;
+        
+        new bool RemoveBuff<T>(BuffEntity entity,T buff) where T :struct, IBuffDataComponent;
+
+        new bool HasBuff<T>(BuffEntity entity, T buff) where T :struct, IBuffDataComponent;
+    }
+}
