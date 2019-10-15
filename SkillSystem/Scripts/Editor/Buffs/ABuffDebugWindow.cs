7a259c61d842ab9a93780436adc1351132b2ec6b
Icarus <1375400884@qq.com>
2019-10-16 02:30:47 +0800

    !ADO 改名 BuffDebugWindow -> ABuffDebugWindow 同时变为抽象类

---
+++ SkillSystem/Scripts/Editor/Buffs/ABuffDebugWindow.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+using CabinIcarus.IcSkillSystem.Runtime.Buffs;
+using CabinIcarus.IcSkillSystem.Runtime.Buffs.Entitys;
+using CabinIcarus.IcSkillSystem.Runtime.Buffs.Systems.Interfaces;
+using UnityEditor;
+using EditorGUI = UnityEditor.EditorGUI;
+using EditorGUILayout = UnityEditor.EditorGUILayout;
+
+namespace DefaultNamespace
+{
+    public abstract class ABuffDebugWindow<TEntity> : UnityEditor.EditorWindow where TEntity : IIcSkSEntity
+    {
+        public static IIcSkSEntityManager<IBuffSystem, TEntity> EntityManager;
+        
+        private void OnGUI()
+        {
+            if (EntityManager == null)
+            {
+                EditorGUILayout.HelpBox("please set EntityManager -> BuffDebugWindow.EntityManager",
+                    MessageType.Warning);
+                return;
+            }
+
+            var entitys = EntityManager.Entitys;
+
+            EditorGUILayout.LabelField("Entity count:" + entitys.Count);
+
+            EditorGUI.indentLevel++;
+            foreach (var entity in entitys)
+            {
+                EditorGUILayout.LabelField($"{entity} Buff Count:" +
+                                           EntityManager.BuffManager.GetAllBuff(entity).Count());
+            }
+
+            EditorGUI.indentLevel--;
+        }
+    }
+}
