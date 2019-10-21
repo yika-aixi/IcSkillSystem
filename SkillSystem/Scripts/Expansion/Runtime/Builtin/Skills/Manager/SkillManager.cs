18e9f024724ea7998f2cafc98190c7ddc8f9e8e4
Icarus <1375400884@qq.com>
2019-10-21 19:21:29 +0800

    !R 开启SkillManager

--- SkillSystem/Scripts/Expansion/Runtime/Builtin/Skills/Manager/SkillManager.cs
+++ SkillSystem/Scripts/Expansion/Runtime/Builtin/Skills/Manager/SkillManager.cs
@@ -1,39 +1,39 @@
-﻿//using System.Collections.Generic;
-//using CabinIcarus.IcSkillSystem.Runtime.Buffs.Entitys;
-//using CabinIcarus.IcSkillSystem.Runtime.Skills.Components;
-//using CabinIcarus.IcSkillSystem.Runtime.Skills.Manager;
-//using CabinIcarus.IcSkillSystem.Runtime.Skills.Systems;
-//
-//namespace SkillSystem.SkillSystem.Scripts.Expansion.Runtime.Builtin.Skills.Manager
-//{
-//    public class SkillManager:ISkillManager
-//    {
-//        private List<ISkillExecuteSystem> _skillSystem;
-//
-//        public SkillManager()
+﻿using System.Collections.Generic;
+using CabinIcarus.IcSkillSystem.Runtime.Buffs.Entitys;
+using CabinIcarus.IcSkillSystem.Runtime.Skills.Components;
+using CabinIcarus.IcSkillSystem.Runtime.Skills.Manager;
+using CabinIcarus.IcSkillSystem.Runtime.Skills.Systems;
+
+namespace SkillSystem.SkillSystem.Scripts.Expansion.Runtime.Builtin.Skills.Manager
+{
+    public class SkillManager:ISkillManager
+    {
+        private List<ISkillExecuteSystem> _skillSystem;
+
+        public SkillManager()
+        {
+            _skillSystem = new List<ISkillExecuteSystem>();
+        }
+
+        public void UseSkill(IIcSkSEntity icSkSEntity, ISkillDataComponent skill)
+        {
+            foreach (var system in _skillSystem)
+            {
+                if (system.Filter(icSkSEntity,skill))
+                {
+                    system.Execute(icSkSEntity,skill);
+                }
+            }
+        }
+
+//        public void RemoveSkill(IEntity entity, ISkillDataComponent skill)
 //        {
-//            _skillSystem = new List<ISkillExecuteSystem>();
 //        }
-//
-//        public void UseSkill(IIcSkSEntity icSkSEntity, ISkillDataComponent skill)
-//        {
-//            foreach (var system in _skillSystem)
-//            {
-//                if (system.Filter(icSkSEntity,skill))
-//                {
-//                    system.Execute(icSkSEntity,skill);
-//                }
-//            }
-//        }
-//
-////        public void RemoveSkill(IEntity entity, ISkillDataComponent skill)
-////        {
-////        }
-//
-//        public ISkillManager AddSkillSystem(ISkillExecuteSystem skillSystem)
-//        {
-//            _skillSystem.Add(skillSystem);
-//            return this;
-//        }
-//    }
-//}
+
+        public ISkillManager AddSkillSystem(ISkillExecuteSystem skillSystem)
+        {
+            _skillSystem.Add(skillSystem);
+            return this;
+        }
+    }
+}
