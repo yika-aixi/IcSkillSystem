4125e47306a5698dbb92111050515d8a8947f3be
Icarus <1375400884@qq.com>
2019-11-15 18:02:00 +0800

    !O 抽出一个`Base`,`Base`下的类不能引用`SkillSystem`

---
+++ SkillSystem/Scripts/Expansion/Runtime/Builtin/Skills/Component/SkillBehaveComponent.cs
@@ -0,0 +1,18 @@
+﻿//手动滑稽,滑稽脸
+//ヾ(•ω•`)o
+//https://www.ykls.app
+//2019年11月15日-16:59
+//CabinIcarus.IcSkillSystem.Expansion.Runtime
+
+using CabinIcarus.IcSkillSystem.xNode_Group;
+
+namespace SkillSystem.SkillSystem.Scripts.Expansion.Runtime.Builtin.Skills.Component
+{
+    /// <summary>
+    /// 技能行为
+    /// </summary>
+    public class SkillBehaveComponent
+    {
+        public IcSkillGroup Behave;
+    }
+}
