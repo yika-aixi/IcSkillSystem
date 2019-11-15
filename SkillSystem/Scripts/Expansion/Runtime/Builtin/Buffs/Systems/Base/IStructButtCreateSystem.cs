25a9c9d5f4861b48d23430eea045d9ebbf37d43d
Icarus <1375400884@qq.com>
2019-11-15 17:27:30 +0800

    !O 结构整理,第一次,可能回出现重复引用问题,需要再细分

---
+++ SkillSystem/Scripts/Expansion/Runtime/Builtin/Buffs/Systems/Base/IStructButtCreateSystem.cs
@@ -0,0 +1,6 @@
+﻿namespace SkillSystem.SkillSystem.Scripts.Expansion.Runtime.Builtin.Buffs.Systems.Base
+{
+    public interface IStructButtCreateSystem
+    {
+    }
+}
--- xNode_IcSkill/Runtime/xNode_Group/IcSkillGroup.cs
+++ xNode_IcSkill/Runtime/xNode_Group/IcSkillGroup.cs
@@ -5,6 +5,7 @@
 //2019年09月19日-21:25
 //Assembly-CSharp
 
+using System;
 using CabinIcarus.IcSkillSystem.Runtime.xNode_NPBehave_Node;
 using NPBehave;
 using UnityEngine;
@@ -16,6 +17,23 @@ namespace CabinIcarus.IcSkillSystem.xNode_Group
     [CreateAssetMenu(fileName = "New IcSkill Group",menuName = "CabinIcarus/IcSkillSystem/Group")]
     public class IcSkillGroup:NodeGraph
     {
+        private GameObject _owner;
+
+        public GameObject Owner
+        {
+            get
+            {
+                if (!_owner)
+                {
+                    throw new NullReferenceException("no set Owner");
+                }
+
+                return _owner;
+            }
+
+            set => _owner = value;
+        }
+
         /// <summary>
         /// 开始,返回Node
         /// </summary>
