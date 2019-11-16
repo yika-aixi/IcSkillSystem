bf0b9267841063a3cc883ba778f34d31ee7eab90
Icarus <1375400884@qq.com>
2019-11-16 17:09:51 +0800

    !B(Node) 创建黑板每个node实例之应该创建一个

--- xNode_IcSkill/Runtime/xNode_NPBehave_Node/Blackboard/CreateBlackboardNode.cs
+++ xNode_IcSkill/Runtime/xNode_NPBehave_Node/Blackboard/CreateBlackboardNode.cs
@@ -10,14 +10,18 @@ namespace CabinIcarus.IcSkillSystem.Runtime.xNode_NPBehave_Node
         [Input(ShowBackingValue.Never,ConnectionType.Override,TypeConstraint.Inherited)]
         protected Clock Clock;
 
+        private Blackboard _black;
+        
         protected override Blackboard GetOutValue()
         {
             var clock = GetInputValue(nameof(Clock), Clock);
 
-            if (clock != null) 
-                return new Blackboard(clock);
+            if (clock != null)
+                // 每个node 之应该创建一个
+                if (_black == null)
+                    _black = new Blackboard(clock);
 
-            return null;
+            return _black;
         }
     }
 }
