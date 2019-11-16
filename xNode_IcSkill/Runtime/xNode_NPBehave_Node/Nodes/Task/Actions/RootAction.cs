719ae788b84b578cde344346e26883ab057a0bbf
Icarus <1375400884@qq.com>
2019-11-16 17:13:31 +0800

    !A(Node) Root Action 测试,目前存在问题

---
+++ xNode_IcSkill/Runtime/xNode_NPBehave_Node/Nodes/Task/Actions/RootAction.cs
@@ -0,0 +1,31 @@
+﻿using NPBehave;
+using UnityEngine;
+
+namespace CabinIcarus.IcSkillSystem.Runtime.xNode_NPBehave_Node.Tasks
+{
+    [CreateNodeMenu("CabinIcarus/IcSkillSystem/Behave Nodes/Task/Actions/Root Start Or Stop")]
+    public class RootAction:ANPBehaveNode<Action>
+    {
+        [SerializeField]
+        private bool _start;
+        
+        protected override Action GetOutValue()
+        {
+            return new Action(() =>
+            {
+                var root = OutValue.RootNode;
+                
+                if (_start)
+                {
+                    root.Start();
+                }
+                else
+                {
+                    root.Stop();
+                }
+
+                return false;
+            });
+        }
+    }
+}
