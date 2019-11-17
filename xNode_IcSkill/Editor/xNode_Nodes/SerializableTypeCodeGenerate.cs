dc9864d991d24d9f832da48c5f1e19ce8df80329
Icarus <1375400884@qq.com>
2019-11-18 03:37:41 +0800

    !W 代码生成器

--- xNode_IcSkill/Editor/xNode_Nodes/SerializableTypeCodeGenerate.cs
+++ xNode_IcSkill/Editor/xNode_Nodes/SerializableTypeCodeGenerate.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using CabinIcarus.IcSkillSystem.Editor.xNode_NPBehave_Node.Utils;
+using CabinIcarus.IcSkillSystem.SkillSystem.Runtime.Utils;
 using UnityEditor;
 using UnityEngine;
 using Object = UnityEngine.Object;
@@ -24,7 +25,10 @@ namespace CabinIcarus.IcSkillSystem.Editor.xNode_Nodes
 
         private static string _templateContent;
 
-        private static List<string> _generateAssemblys;
+        private static Dictionary<string,bool> _generateTypeAqNameMap;
+        private static Dictionary<Type,bool> _generateTypeMap;
+        private static List<bool> _groupStates;
+        private static IEnumerable<IGrouping<string, KeyValuePair<Type, bool>>> _assemblyGroup;
         
         private static void _loadTemple()
         {
@@ -62,28 +66,116 @@ namespace CabinIcarus.IcSkillSystem.Editor.xNode_Nodes
                 }
                 return str;
             }
+            
             set => EditorPrefs.SetString(_getKey(nameof(NameSpace)),value);
         }
 
         [MenuItem("Icarus/IcSkillSystem/ValueNode Code Generate")]
         static void _open()
         {
-            var wind = EditorWindow.GetWindow<SerializableTypeCodeGenerate>();
+            var wind = GetWindow<SerializableTypeCodeGenerate>();
             wind.titleContent = new GUIContent("ValueNode Generate");
-            _generateAssemblys = new List<string>();
-            _generateAssemblys =
-                JsonUtility.FromJson<List<string>>(EditorPrefs.GetString(_getKey(nameof(_generateAssemblys))));
+            _init();
+        }
+
+        private static void _init()
+        {
+            _generateTypeAqNameMap = new Dictionary<string, bool>();
+            _generateTypeAqNameMap =
+                SerializationUtil.ToValue<Dictionary<string, bool>>(
+                    EditorPrefs.GetString(_getKey(nameof(_generateTypeAqNameMap))));
+
+            var runtimeTypes = TypeUtil.AllTypes
+                .Where(x => x.CustomAttributes.All(y => y.AttributeType != typeof(ObsoleteAttribute)))
+                .Where(x => !x.IsPointer)
+                .Where(x => !typeof(Delegate).IsAssignableFrom(x))
+                .Where(x => !typeof(Exception).IsAssignableFrom(x))
+                .Where(x => !typeof(Attribute).IsAssignableFrom(x))
+                .Where(x => !x.IsGenericType)
+                .Where(x => !x.IsAbstract)
+                .Where(x =>
+                {
+                    if (x.IsSealed)
+                    {
+                        return x.IsSealed && x.IsPublic || x.IsPublic;
+                    }
+
+                    return x.IsPublic;
+                })
+                .Where(x => x.IsValueType || x.IsEnum ||
+                            x.IsClass && x.CustomAttributes.Any(y => y.AttributeType == typeof(SerializableAttribute)) ||
+                            typeof(Object).IsAssignableFrom(x)).ToList();
+            
+            if (_generateTypeAqNameMap == null || _generateTypeAqNameMap.Count != runtimeTypes.Count)
+            {
+                if (_generateTypeAqNameMap == null)
+                {
+                    _generateTypeAqNameMap = new Dictionary<string, bool>();
+                }
+                
+                foreach (var runtimeType in runtimeTypes)
+                {
+                    if (runtimeType.AssemblyQualifiedName == null)
+                    {
+                        Debug.LogError(runtimeType);
+                        continue;
+                    }
+                    
+                    if (_generateTypeAqNameMap.ContainsKey(runtimeType.AssemblyQualifiedName))
+                    {
+                        continue;
+                    }
+                    _generateTypeAqNameMap.Add(runtimeType.AssemblyQualifiedName, true);
+                }
+            }
+
+            if (_generateTypeMap == null)
+            {
+                bool exitMissing = false;
+                
+                _generateTypeMap = _generateTypeAqNameMap.Where(x =>
+                {
+                    if (Type.GetType(x.Key) != null)
+                    {
+                        return true;
+                    }
+                    
+                    Debug.LogWarning($"Missing Type '{x.Key}'");
+
+                    exitMissing = true;
+                    
+                    return false;
+                }).ToDictionary(x => Type.GetType(x.Key), x => x.Value);
+
+                if (exitMissing)
+                {
+                    _save();
+                }
+                
+                _assemblyGroup = _generateTypeMap.GroupBy(x => x.Key.Assembly.FullName);
+                _groupStates = new List<bool>();
+
+                foreach (var unused in _assemblyGroup)
+                {
+                    _groupStates.Add(false);
+                }
+            }
         }
 
         private void OnEnable()
         {
             _loadTemple();
+            
+            if (_generateTypeMap == null)
+            {
+                _init();
+            }
         }
 
         private static string _getKey(string key)
         {
             return $"{nameof(SerializableTypeCodeGenerate)}.{key}";
-;        }
+        }
 
         private bool _force = false;
 
@@ -111,9 +203,11 @@ namespace CabinIcarus.IcSkillSystem.Editor.xNode_Nodes
             
             _drawGenerate();
         }
-
+        
         private void _drawGenerate()
         {
+            _drawAssemblyEnableOrClose();
+
             if (GUILayout.Button("Generate"))
             {
                 if (!Directory.Exists(GenerateSavePath))
@@ -122,47 +216,31 @@ namespace CabinIcarus.IcSkillSystem.Editor.xNode_Nodes
                 }
                 
                 EditorUtility.DisplayProgressBar("Generate ing", string.Empty, 0);
-                
-                var runtimeTypes = TypeUtil.UnityRuntimeTypes
-                    .Where(x => x.CustomAttributes.All(y => y.AttributeType != typeof(ObsoleteAttribute)))
-                    .Where(x => !x.IsPointer)
-                    .Where(x => !typeof(Delegate).IsAssignableFrom(x))
-                    .Where(x => !typeof(Exception).IsAssignableFrom(x))
-                    .Where(x => !typeof(Attribute).IsAssignableFrom(x))
-                    .Where(x => !x.IsGenericType)
-                    .Where(x => !x.IsAbstract)
-                    .Where(x =>
-                    {
-                        if (x.IsSealed)
-                        {
-                            return x.IsSealed && x.IsPublic || x.IsPublic;
-                        }
 
-                        return x.IsPublic;
-                    })
-                    .Where(x => x.IsValueType || x.IsEnum || x.IsClass && x.CustomAttributes.Any(y => y.AttributeType == typeof(SerializableAttribute)) ||
-                                typeof(Object).IsAssignableFrom(x)).ToList();
-
-
-                runtimeTypes.Add(typeof(int));
-                runtimeTypes.Add(typeof(long));
-                runtimeTypes.Add(typeof(float));
-                runtimeTypes.Add(typeof(bool));
-                runtimeTypes.Add(typeof(string));
-
-                var count = runtimeTypes.Count;
+                var count = _generateTypeMap.Count;
                 
                 int i = 0;
                 
-                foreach (var runtimeType in runtimeTypes)
+                foreach (var item in _generateTypeMap)
                 {
+                    var runtimeType = item.Key;
                     EditorUtility.DisplayProgressBar("Generate ing", runtimeType.FullName, i / (float) count);
+                    var typeName = runtimeType.Name.Split('.').Last();
+                    var fileName = $"{typeName}ValueNode.cs";
+                    var path = Path.Combine(GenerateSavePath, fileName);
+                    
+                    if (!item.Value)
+                    {
+                        if (File.Exists(path))
+                        {
+                            File.Delete(path);
+                        }
+                        ++i;
+                        continue;
+                    }
+                    
                     if (_isSerializableType(runtimeType))
                     {
-                        var typeName = runtimeType.Name.Split('.').Last();
-                        var fileName = $"{typeName}ValueNode.cs";
-                        var path = Path.Combine(GenerateSavePath, fileName);
-
                         if (!_force)
                         {
                             if (File.Exists(path))
@@ -197,6 +275,91 @@ namespace CabinIcarus.IcSkillSystem.Editor.xNode_Nodes
             }
         }
 
+        private bool _showAssemblyEnableOrClose;
+        private bool _temp;
+        private Vector2 _showAssemblyEnableOrClosePos;
+        private void _drawAssemblyEnableOrClose()
+        {
+            _showAssemblyEnableOrClose = EditorGUILayout.Foldout(_showAssemblyEnableOrClose, "Assembly Generate Enable Or Close",true);
+
+            if (!_showAssemblyEnableOrClose)
+            {
+                return;
+            }
+            _assemblyGroup = _generateTypeMap.GroupBy(x => x.Key.Assembly.GetName().Name);
+            EditorGUI.indentLevel++;
+            _showAssemblyEnableOrClosePos = EditorGUILayout.BeginScrollView(_showAssemblyEnableOrClosePos);
+            {
+                int index = 0;
+                foreach (var grouping in _assemblyGroup)
+                {
+                    var list = grouping.ToList();
+                    
+                    EditorGUILayout.BeginHorizontal();
+                    {
+                        _groupStates[index] = EditorGUILayout.Foldout(_groupStates[index], grouping.Key, true);
+
+                        var t = list.All(x => x.Value);
+                        var f = list.All(x => x.Value == false);
+
+                        if (!t && !f)
+                        {
+                            EditorGUI.showMixedValue = true;
+                        }
+                        else
+                        {
+                            _temp = t;
+                        }
+                        
+                        EditorGUI.BeginChangeCheck();
+                        {
+                            _temp = EditorGUILayout.Toggle(_temp);
+                        }
+                        if (EditorGUI.EndChangeCheck())
+                        {
+                            foreach (var item in list)
+                            {
+                                _generateTypeMap[item.Key] = _temp;
+                            }
+
+                            _save();
+                        }
+                        
+                        EditorGUI.showMixedValue = false;
+                    }
+                    EditorGUILayout.EndHorizontal();
+                    if (_groupStates[index])
+                    {
+                        EditorGUI.indentLevel++;
+                        {
+                            foreach (var item in list)
+                            {
+                                EditorGUI.BeginChangeCheck();
+                                {
+                                    _temp = EditorGUILayout.ToggleLeft(item.Key.FullName, item.Value);
+                                }
+                                if (EditorGUI.EndChangeCheck())
+                                {
+                                    _generateTypeMap[item.Key] = _temp;
+                                    _save();
+                                }
+                            }
+                        }
+                        EditorGUI.indentLevel--;
+                    }
+                    index ++;
+                }
+            }
+            EditorGUILayout.EndScrollView();
+            EditorGUI.indentLevel--;
+        }
+
+        static void _save()
+        {
+            var aqNameMap = _generateTypeMap.ToDictionary(x => x.Key.AssemblyQualifiedName, x => x.Value);
+            EditorPrefs.SetString(_getKey(nameof(_generateTypeAqNameMap)),SerializationUtil.ToString(aqNameMap));
+        }
+        
         bool _isSerializableType(Type type)
         {
             return true;
