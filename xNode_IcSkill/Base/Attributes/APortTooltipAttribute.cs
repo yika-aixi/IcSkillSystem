7ea4aec16fa4789004409c114f53043a77ec20df
Icarus <1375400884@qq.com>
2019-11-19 00:02:51 +0800

    !W

--- xNode_IcSkill/Base/Attributes/APortTooltipAttribute.cs
+++ xNode_IcSkill/Base/Attributes/APortTooltipAttribute.cs
@@ -2,6 +2,7 @@
 
 namespace CabinIcarus.IcSkillSystem.Runtime.xNode_NPBehave_Node.Attributes
 {
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field,AllowMultiple = false,Inherited = false)]
     public class APortTooltipAttribute:Attribute
     {
     }
