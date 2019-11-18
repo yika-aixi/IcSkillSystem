d5447154f9450ca02ba139be15aebb7cbb37804a
Icarus <1375400884@qq.com>
2019-11-19 00:04:24 +0800

    !W(Skill Group Inspector) 节点的顺序调整 *

--- xNode_IcSkill/Editor/xNode_Group/IcSkillGroupInspector.cs
+++ xNode_IcSkill/Editor/xNode_Group/IcSkillGroupInspector.cs
@@ -1,9 +1,14 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using CabinIcarus.IcSkillSystem.Editor.xNode_NPBehave_Node.Utils;
 using SkillSystem.xNode_IcSkill.Editor.Util;
 using UnityEditor;
+using UnityEditorInternal;
 using UnityEngine;
+using XNode;
+using XNodeEditor;
 using Object = UnityEngine.Object;
 
 namespace CabinIcarus.IcSkillSystem.xNode_Group.Editor
@@ -21,7 +26,7 @@ namespace CabinIcarus.IcSkillSystem.xNode_Group.Editor
         private static ValueEditPopupWindow _ValueEditPopup;
         private static SimpleTypeSelectPopupWindow _SimpleTypeSelect;
         private Rect _rect;
-
+        
         private void OnEnable()
         {
             if (_types == null)
@@ -52,13 +57,143 @@ namespace CabinIcarus.IcSkillSystem.xNode_Group.Editor
 
             serializedObject.Update();
             {
+                _drawNodes();
                 _drawBlackboardVar();
             }
             serializedObject.ApplyModifiedProperties();
         }
 
+        #region Nodes
+
+        private SerializedProperty _nodesSer;
+        private SerializedProperty _portsSer;
+        private bool _showNodes;
+        Dictionary<int,bool> _nodeStateMap = new Dictionary<int, bool>();
+        Dictionary<int,ReorderableList> _connectionsMap = new Dictionary<int, ReorderableList>();
+        private Object _lastTarget;
+        private void _drawNodes()
+        {
+            if (_nodesSer == null)
+            {
+                _nodesSer = serializedObject.FindProperty("nodes");
+            }
+
+            if (_lastTarget != target)
+            {
+                _nodeStateMap.Clear();
+                _connectionsMap.Clear();
+                _lastTarget = target;
+            }
+
+            _showNodes = EditorGUILayout.Foldout(_showNodes, "Nodes", true);
+
+            if (!_showNodes)
+            {
+                return;
+            }
+            EditorGUI.indentLevel++;
+            {
+                for (var i = 0; i < _nodesSer.arraySize; i++)
+                {
+                    if (!_nodeStateMap.ContainsKey(i))
+                    {
+                        _nodeStateMap.Add(i, false);
+                        _connectionsMap.Add(i,null);
+                    }
+
+                    var node = _nodesSer.GetArrayElementAtIndex(i);
+
+                    _nodeStateMap[i] = EditorGUILayout.Foldout(_nodeStateMap[i], node.objectReferenceValue.name, true);
+
+                    if (_nodeStateMap[i])
+                    {
+                        var nodeO = new SerializedObject(node.objectReferenceValue);
+                        SerializedProperty portsSer = nodeO.FindProperty("ports");
+                        SerializedProperty valuesSer = portsSer.FindPropertyRelative("values");
+                        
+                        for (var j = 0; j < valuesSer.arraySize; j++)
+                        {
+                            var property = valuesSer.GetArrayElementAtIndex(j);
+
+                            var connections = property.FindPropertyRelative("connections");
+                            
+                            if (property.FindPropertyRelative("_direction").enumValueIndex == (int) NodePort.IO.Input)
+                            {
+                                EditorGUI.indentLevel++;
+                                {
+                                    if (connections.arraySize > 1)
+                                    {
+                                        ReorderableList list = _connectionsMap[i];
+                                        if (list == null)
+                                        {
+                                            list = new ReorderableList(nodeO, connections, true, true, 
+                                                false, false);
+                                            list.drawHeaderCallback = rect => EditorGUI.LabelField(rect,"Input");
+                                            list.drawElementCallback = (rect, index, active, focused) =>
+                                            {
+                                                var output = connections.GetArrayElementAtIndex(index);
+                                                var targetNode = output.FindPropertyRelative("node");
+                                                EditorGUI.LabelField(rect, $"{index + 1} : {targetNode.objectReferenceValue.name}");
+                                            };
+                                            list.onReorderCallback = _ =>
+                                            {
+                                                _save();
+                                            }; 
+                                            _connectionsMap[i] = list;
+                                        }
+                                        
+                                        EditorGUI.indentLevel-=2;
+                                        {
+                                            list.DoLayoutList();
+                                        }
+                                        EditorGUI.indentLevel+=2;
+                                    }
+                                    else
+                                    {
+                                        if (connections.arraySize > 0)
+                                        {
+                                            var output = connections.GetArrayElementAtIndex(0);
+                                            var targetNode = output.FindPropertyRelative("node");
+                                            EditorGUILayout.LabelField(targetNode.objectReferenceValue.name);
+                                        }
+                                        else
+                                        {
+                                            EditorGUILayout.HelpBox("No Connection",MessageType.Info);
+                                        }
+
+                                        
+                                    }
+                                }
+                                EditorGUI.indentLevel--;
+                            }
+                            else
+                            {
+                                EditorGUILayout.LabelField("OutPut");
+                                EditorGUI.indentLevel++;
+                                {
+                                    if (connections.arraySize > 0)
+                                    {
+                                        var output = connections.GetArrayElementAtIndex(0);
+                                        var targetNode = output.FindPropertyRelative("node");
+                                        EditorGUILayout.LabelField(targetNode.objectReferenceValue.name);
+                                    }
+                                }
+                                EditorGUI.indentLevel--;
+                            }
+                        }
+                    }
+                }
+            }
+            EditorGUI.indentLevel--;
+        }
+
+        #endregion
+
+        #region BloackBoard Variable
+
         private bool _showBlackboard;
-        
+        private ReorderableList list1;
+
         private void _drawBlackboardVar()
         {
             _showBlackboard = EditorGUILayout.Foldout(_showBlackboard, "Blackboard Variable",true);
@@ -235,5 +370,7 @@ namespace CabinIcarus.IcSkillSystem.xNode_Group.Editor
                 _save();
             }
         }
+
+        #endregion
     }
 }
