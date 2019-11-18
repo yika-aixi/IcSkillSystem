6638360321027b8a69ee7418ab426eabcae1afde
Icarus <1375400884@qq.com>
2019-11-18 18:36:40 +0800

    !A(Node) 坐标设置Node

---
+++ SkillSystem/Scripts/Expansion/Runtime/Builtin/Nodes/MoveNodes/AMoveNode.cs
@@ -0,0 +1,20 @@
+﻿using CabinIcarus.IcSkillSystem.Runtime.xNode_NPBehave_Node;
+using CabinIcarus.IcSkillSystem.Runtime.xNode_NPBehave_Node.Attributes;
+using NPBehave;
+using UnityEngine;
+
+namespace CabinIcarus.IcSkillSystem.Expansion.Runtime.Builtin.Nodes
+{
+    public abstract class AMoveNode:ANPBehaveNode<Action>
+    {
+        [SerializeField]
+        [Input(ShowBackingValue.Always,ConnectionType.Override,TypeConstraint.Strict)]
+        [PortTooltipMethodOrPropertyGet(nameof(Tooltip))]
+        private Vector3 _destination;
+
+        protected Vector3 Destination => GetInputValue(nameof(_destination), _destination);
+        protected Transform Tran => SkillGroup.Owner.transform;
+        
+        protected virtual string Tooltip => "Destination";
+    }
+}
---
+++ SkillSystem/Scripts/Expansion/Runtime/Builtin/Nodes/MoveNodes/SetPositionNode.cs
@@ -0,0 +1,18 @@
+﻿using NPBehave;
+
+namespace CabinIcarus.IcSkillSystem.Expansion.Runtime.Builtin.Nodes
+{
+    [CreateNodeMenu("CabinIcarus/IcSkillSystem/Behave Nodes/Task/Actions/Move/Set Position")]
+    public class SetPositionNode:AMoveNode
+    {
+        protected override Action GetOutValue()
+        {
+            return new Action(_setPos);
+        }
+
+        private void _setPos()
+        {
+            Tran.position = Destination;
+        }
+    }
+}
