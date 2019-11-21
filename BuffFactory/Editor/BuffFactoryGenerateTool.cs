38f3d0857f73907ff22c240f4a55c4d29250e2a9
Icarus <1375400884@qq.com>
2019-11-21 18:16:32 +0800

    !A Buff Factory 提供给可视化使用,代码生成器未完成,哈哈

---
+++ BuffFactory/Editor/BuffFactoryGenerateTool.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEditor.Compilation;
+using UnityEditorInternal;
+using UnityEngine;
+
+namespace CabinIcarus.IcSkillSystem.BuffFactory.Tool
+{
+    public class BuffFactoryGenerateTool : UnityEditor.EditorWindow
+    {
+        [UnityEditor.MenuItem("Icarus/Skill System/Buff Factory Generate Tool")]
+        private static void ShowWindow()
+        {
+            var window = GetWindow<BuffFactoryGenerateTool>();
+            window.titleContent = new UnityEngine.GUIContent("Buff Factory Generate Tool");
+            window.Show();
+        }
+        
+        private string[] _codeLines;
+
+        private List<string> _usings;
+
+        private List<string> _switchs;
+
+        private string _savePath;
+        
+        private void OnEnable()
+        {
+            var location = AssetDatabase.LoadAssetAtPath<TextAsset>("BuffFactoryGenerateLocation");
+            _savePath = AssetDatabase.GetAssetPath(location);
+
+            if (!string.IsNullOrWhiteSpace(_savePath))
+            {
+                _savePath = Path.GetDirectoryName(_savePath);
+            }
+            
+            var assembly = CompilationPipeline.GetAssemblies(AssembliesType.Player);
+//            importer.
+        }
+
+        private void OnGUI()
+        {
+        }
+    }
+}
---
+++ BuffFactory/Runtime/BuffFactory.cs
@@ -0,0 +1,41 @@
+﻿using CabinIcarus.IcSkillSystem.Expansion.Runtime.Buffs.Components;
+using CabinIcarus.IcSkillSystem.Runtime.Buffs.Components;
+using CabinIcarus.IcSkillSystem.Runtime.Buffs.Entitys;
+
+//### Using
+                
+//###
+
+namespace  CabinIcarus.IcSkillSystem.BuffFactory
+{
+    public static class BuffFactory
+    {
+        public static void AddBuff(IIcSkSEntityManager<IIcSkSEntity> entityManager, IIcSkSEntity entity,string buffType,
+            IBuffDataComponent buff)
+        {
+            AddBuff<IIcSkSEntity>(entityManager, entity,buffType, buff);
+        }
+        
+        public static void AddBuff<TE>(IIcSkSEntityManager<TE> entityManager, TE entity,string buffType,
+            IBuffDataComponent buff) where TE : IIcSkSEntity
+        {
+            if (entityManager is IStructIcSkSEntityManager<TE> structIcSkSEntityManager)
+            {
+                //### Code
+                if (buffType == typeof(Damage).FullName)
+                {
+                    structIcSkSEntityManager.AddBuff(entity,(Damage) buff);   
+                }
+                else if (buffType == typeof(DeathStruct).FullName)
+                {
+                    structIcSkSEntityManager.AddBuff(entity,(DeathStruct) buff);
+                }
+                else if (buffType == typeof(Mechanics).FullName)
+                {
+                    structIcSkSEntityManager.AddBuff(entity,(Mechanics) buff);
+                }
+                //###
+            }
+        }
+    }
+}
