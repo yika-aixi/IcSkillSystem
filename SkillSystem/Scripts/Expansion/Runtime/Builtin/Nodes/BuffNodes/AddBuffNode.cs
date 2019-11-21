5fc21456ed708a89233551a5a10ecad93ece58d9
Icarus <1375400884@qq.com>
2019-11-21 12:15:21 +0800

    !A(Node) Buff Node exist Box action

---
+++ SkillSystem/Scripts/Expansion/Runtime/Builtin/Nodes/BuffNodes/AddBuffNode.cs
@@ -0,0 +1,12 @@
+﻿using NPBehave;
+
+namespace CabinIcarus.IcSkillSystem.Expansion.Runtime.Builtin.Nodes.BuffNodes
+{
+    public class AddBuffNode:ABuffNode
+    {
+        protected override void Execute()
+        {
+            
+        }
+    }
+}
