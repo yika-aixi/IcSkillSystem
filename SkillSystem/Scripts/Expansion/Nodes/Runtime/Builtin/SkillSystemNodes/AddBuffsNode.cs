58645f53d3b6d0cc87c428f3b8d34de97e7240cd
Icarus <1375400884@qq.com>
2019-11-21 18:15:16 +0800

    !AD 从`Runtime/Builtin`抽离到`Nodes/Runtime/Builtin`

---
+++ SkillSystem/Scripts/Expansion/Nodes/Runtime/Builtin/SkillSystemNodes/AddBuffsNode.cs
@@ -0,0 +1,28 @@
+﻿using NPBehave;
+
+namespace CabinIcarus.IcSkillSystem.Expansion.Runtime.Builtin.Nodes.BuffNodes
+{
+    public class AddBuffsNode:ASKNode
+    {
+        [Input(ShowBackingValue.Unconnected,ConnectionType.Multiple,TypeConstraint.Inherited)]
+        private string _buffs;
+
+        private string[] _getBuffs => GetInputValues<string>(nameof(_buffs), null);
+        
+        protected override Task GetTask()
+        {
+            return new Action(_addBuff);
+        }
+
+        private void _addBuff()
+        {
+            if (_getBuffs != null)
+            {
+                foreach (var buff in _getBuffs)
+                {
+                    
+                }
+            }
+        }
+    }
+}
