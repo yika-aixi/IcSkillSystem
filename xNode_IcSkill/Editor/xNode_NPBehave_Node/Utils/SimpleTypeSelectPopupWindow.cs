01d2bfb23ccd8d31d573638595c2524becdc75f2
Icarus <1375400884@qq.com>
2019-11-21 23:36:14 +0800

    !O(SimpleTypeSelectPopupWindow) 从自定义实现的列表改为`Tree View`实现

--- xNode_IcSkill/Editor/xNode_NPBehave_Node/Utils/SimpleTypeSelectPopupWindow.cs
+++ xNode_IcSkill/Editor/xNode_NPBehave_Node/Utils/SimpleTypeSelectPopupWindow.cs
@@ -2,25 +2,120 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using System.Text.RegularExpressions;
 using UnityEditor;
 using UnityEditor.IMGUI.Controls;
 using UnityEngine;
 
 namespace CabinIcarus.IcSkillSystem.Editor.xNode_NPBehave_Node.Utils
 {
+    internal class TypeTreeView : TreeView
+    {
+        class TypeItem:TreeViewItem
+        {
+            public Type Type { get; }
+
+            public TypeItem(int id, int depth, string displayName, Type type) : base(id, depth, displayName)
+            {
+                Type = type;
+            }
+        }
+        private readonly IEnumerable<Type> types;
+        
+        public Action<Type> OnSelect;
+
+        public TypeTreeView(IEnumerable<Type> types,TreeViewState state, MultiColumnHeader multiColumnHeader = null) : base(state, multiColumnHeader)
+        {
+            this.types = types;
+            this.useScrollView = true;
+        }
+
+        protected override bool CanMultiSelect(TreeViewItem item)
+        {
+            if (item is TypeItem typeItem)
+            {
+                OnSelect?.Invoke(typeItem.Type);
+            }
+
+            SetSelection(new List<int>(item.id));
+            
+            return base.CanMultiSelect(item);
+        }
+
+        protected override void DoubleClickedItem(int id)
+        {
+            var item = this.FindItem(id,rootItem);
+
+            if (item.hasChildren)
+            {
+                SetExpanded(id, true);
+            }
+        }
+
+        protected override TreeViewItem BuildRoot()
+        {
+            int depth = -1;
+            TreeViewItem root = new TreeViewItem(-1,depth);
+
+            int id = 0;
+            
+            foreach (var type in types)
+            {
+                var names = type.Assembly.GetName().Name.Split('.');
+                depth = 0;
+
+                TreeViewItem last = root;
+                foreach (var name in names)
+                {
+                    if (last.hasChildren)
+                    {
+                        foreach (var child in last.children)
+                        {
+                            if (child.depth == depth)
+                            {
+                                if (child.displayName == name)
+                                {
+                                    last = child;
+                                    goto end;
+                                }
+                            }
+                        }
+                    }
+
+                    var ch = new TreeViewItem(id, depth, name);
+                    last.AddChild(ch);
+                    last = ch;
+                    id++;
+
+                    end:
+                    depth++;
+                }
+
+                last.AddChild(new TypeItem(id,depth,type.Name,type));
+
+                id++;
+            }
+
+            return root;
+        }
+    }
+    
     public class SimpleTypeSelectPopupWindow : PopupWindowContent
     {
         private readonly bool _focus;
         private Type _baseType;
         private string _ser;
         private SearchField searchField;
+        private TypeTreeView _tree;
         public SimpleTypeSelectPopupWindow(bool focus):this(focus,TypeUtil.GetRuntimeTypes) { }
 
         public SimpleTypeSelectPopupWindow(bool focus,IEnumerable<Type> types)
         {
             this._focus = focus;
             _typeGroup = types.GroupBy(x => x.Assembly);
+            
+            _tree = new TypeTreeView(types,new TreeViewState());
+            _tree.OnSelect = x=> OnChangeTypeSelect?.Invoke(x);
+            _tree.Reload();
         }
 
         public Type BaseType
@@ -64,85 +159,11 @@ namespace CabinIcarus.IcSkillSystem.Editor.xNode_NPBehave_Node.Utils
                 {
                     _state[i1] = false;
                 }
+
+                _tree.searchString = _ser;
             }
 
-            EditorGUILayout.Space();
-            EditorGUILayout.Space();
-            EditorGUILayout.Space();
-            
-            _pos = EditorGUILayout.BeginScrollView(_pos);
-
-            int i = 0;
-            foreach (var group in _typeGroup)
-            {
-                if (_state.Count <= i)
-                {
-                    _state.Add(false);
-                }
-
-                bool isSkip = true;
-                
-                foreach (var valueType in @group)
-                {
-                    if (!_isSkip(valueType))
-                    {
-                        isSkip = false;
-                        break;
-                    }
-                }
-
-                if (isSkip)
-                {
-                    continue;
-                }
-
-                var assemblyPath = group.Key.ConversionAssemblyName();
-                
-                _state[i] = EditorGUILayout.Foldout(_state[i],assemblyPath , true);
-
-                if (_state[i])
-                {
-                    GUILayout.BeginHorizontal();
-                    GUILayout.Space(1*20);
-                    GUILayout.BeginVertical();
-                    foreach (var valueType in @group)
-                    {
-                        if (_isSkip(valueType))
-                        {
-                            continue;
-                        }
-
-                        if (GUILayout.Button(valueType.Name,"label"))
-                        {
-                            OnChangeTypeSelect?.Invoke(valueType);
-                        }
-                    }
-                    GUILayout.EndVertical();
-                    GUILayout.EndHorizontal();
-                }
-                ++i;
-            }
-           
-            EditorGUILayout.EndScrollView();
-        }
-
-        private bool _isSkip(Type valueType)
-        {
-            var typeMatch = BaseType != null && BaseType.IsAssignableFrom(valueType);
-
-            if (!typeMatch)
-            {
-                return true;
-            }
-
-            if (string.IsNullOrWhiteSpace(_ser))
-            {
-                return false;
-            }
-
-            var searchMatch = Regex.IsMatch(valueType.FullName,_ser,RegexOptions.IgnoreCase);
-            
-            return !searchMatch;
+            _tree.OnGUI(new Rect(rect.position + new Vector2(0,25),rect.size - new Vector2(0,25)));
         }
     }
 }
