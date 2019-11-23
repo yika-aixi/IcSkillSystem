88b787b2fada750b57c84cd627be265c4f4df11e
Icarus <1375400884@qq.com>
2019-11-24 04:35:54 +0800

    !A(Node) 技能系统抽象节点

---
+++ xNode_IcSkill/Base/ASkillSNode.cs
@@ -0,0 +1,25 @@
+//创建者:Icarus
+//手动滑稽,滑稽脸
+//ヾ(•ω•`)o
+//https://www.ykls.app
+//2019年11月23日-20:11
+//CabinIcarus.IcSkillSystem.xNodeIc.Base
+
+using CabinIcarus.IcSkillSystem.Runtime.Buffs.Entitys;
+using NPBehave;
+
+namespace CabinIcarus.IcSkillSystem.Runtime.Nodes
+{
+    public abstract class ASkillSNode :ANPBehaveNode<Node>
+    {
+        [Input(ShowBackingValue.Always,ConnectionType.Override,TypeConstraint.Inherited)]
+        private IIcSkSEntityManager<IIcSkSEntity> _entityManager;
+        
+        [Input(ShowBackingValue.Never,ConnectionType.Override,TypeConstraint.Inherited)]
+        private IIcSkSEntity _target;
+
+        protected IIcSkSEntityManager<IIcSkSEntity> EntityManager => GetInputValue(nameof(_entityManager),_entityManager);
+
+        public IIcSkSEntity Target => GetInputValue(nameof(_target),_target);
+    }
+}
