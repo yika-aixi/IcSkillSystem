ae74c6f39711efada5edc1964afba4a7725c76f1
Icarus <1375400884@qq.com>
2019-11-24 04:43:13 +0800

    !A(Node) 存在buff节点,暂时未完成,目前只有存在才会走,后面加入判断,时不存在还是存在才走

---
+++ xNode_IcSkill/Runtime/xNode_NPBehave_Node/Nodes/SkillSystem/Buff/HasBuffConditionNode.cs
@@ -0,0 +1,32 @@
+﻿using NPBehave;
+using UnityEngine;
+
+namespace CabinIcarus.IcSkillSystem.Runtime.Nodes.SkillSystems.Buff
+{
+    [CreateNodeMenu("CabinIcarus/IcSkillSystem/Skill/Buff/Has Condition")]
+    public class HasBuffConditionNode:ABuffNode
+    {
+        [SerializeField,Input(ShowBackingValue.Always,ConnectionType.Override,TypeConstraint.Strict)]
+        private Stops _stops;
+        
+        [SerializeField,Input(ShowBackingValue.Unconnected,ConnectionType.Override,TypeConstraint.Inherited)]
+        protected Node DecorateeNode;
+        
+        protected override Node Execute()
+        {
+            return new Condition(_hasBuff,GetInputValue(nameof(_stops),_stops),GetInputValue(nameof(DecorateeNode),DecorateeNode));
+        }
+
+        private bool _hasBuff()
+        {
+            return BuffFactory.HasBuff(EntityManager, Target, Buff);
+        }
+
+//        private IBuffDataComponent _hasBuff()
+//        {
+//            //todo 暂时先这样写下
+//            Debug.LogError("空值比较 -----------");
+//            return (IBuffDataComponent) Activator.CreateInstance(BuffType);
+//        }
+    }
+}
