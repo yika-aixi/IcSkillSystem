08ca6eaa74898a9fb115aeef58383fafb5cb5593
Icarus <1375400884@qq.com>
2019-11-24 04:36:50 +0800

    !A(ValueNode) 获取图拥有者的`IcSkSEntity`

---
+++ SkillSystem/Scripts/Expansion/Nodes/Runtime/Builtin/ValueNodes/OwnerToBuffEntityValueNode.cs
@@ -0,0 +1,28 @@
+using System;
+using CabinIcarus.IcSkillSystem.Runtime.Buffs.Entitys;
+using CabinIcarus.IcSkillSystem.Runtime.xNode_Nodes;
+using SkillSystem.SkillSystem.Scripts.Expansion.Runtime.Builtin.Entitys;
+
+namespace CabinIcarus.IcSkillSystem.Expansion.Runtime.Builtin.Nodes.ValueNodes
+{
+    [CreateNodeMenu("CabinIcarus/IcSkillSystem/Entity/Go To Entity")]
+    public class OwnerToBuffEntityValueNode:ValueNode
+    {
+        [Input(ShowBackingValue.Always,ConnectionType.Override,TypeConstraint.Inherited)]
+        private IcSkSEntityManager _entityManager;
+        
+        public override Type ValueType { get; } = typeof(IcSkSEntity);
+        
+        protected override object GetOutValue()
+        {
+            var entityManager = GetInputValue(nameof(_entityManager), _entityManager);
+
+            if (entityManager == null)
+            {
+                return new IcSkSEntity(-1);
+            }
+
+            return entityManager.FindBindEntity(SkillGroup.Owner.gameObject);
+        }
+    }
+}
