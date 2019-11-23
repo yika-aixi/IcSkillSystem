0cd6c31d7809bb1778a724ef4c41682d6ca7d3c8
Icarus <1375400884@qq.com>
2019-11-24 04:44:42 +0800

    !WO BuffFactory

--- BuffFactory/Runtime/BuffFactory.cs
+++ BuffFactory/Runtime/BuffFactory.cs
@@ -1,6 +1,9 @@
-﻿using CabinIcarus.IcSkillSystem.Expansion.Runtime.Buffs.Components;
+﻿using System;
+using CabinIcarus.IcSkillSystem.Expansion.Runtime.Buffs.Components;
 using CabinIcarus.IcSkillSystem.Runtime.Buffs.Components;
 using CabinIcarus.IcSkillSystem.Runtime.Buffs.Entitys;
+using SkillSystem.SkillSystem.Scripts.Expansion.Runtime.Builtin.Entitys;
+using UnityEngine;
 
 //### Using
                 
@@ -10,31 +13,101 @@ namespace  CabinIcarus.IcSkillSystem
 {
     public static class BuffFactory
     {
-        public static void AddBuff(IIcSkSEntityManager<IIcSkSEntity> entityManager, IIcSkSEntity entity,string buffType,
-            IBuffDataComponent buff)
+        #region Has Buff
+
+        public static bool HasBuff(IIcSkSEntityManager<IIcSkSEntity> entityManager, IIcSkSEntity entity,IBuffDataComponent buff)
         {
-            AddBuff<IIcSkSEntity>(entityManager, entity,buffType, buff);
+            return HasBuff<IIcSkSEntity>(entityManager,entity, buff);
         }
         
-        public static void AddBuff<TE>(IIcSkSEntityManager<TE> entityManager, TE entity,string buffType,
+        public static bool HasBuff<TE>(IIcSkSEntityManager<TE> entityManager, TE entity,
             IBuffDataComponent buff) where TE : IIcSkSEntity
         {
+            bool result = false;
             if (entityManager is IStructIcSkSEntityManager<TE> structIcSkSEntityManager)
             {
                 //### Code
-                if (buffType == typeof(Damage).FullName)
-                {
-                    structIcSkSEntityManager.AddBuff(entity,(Damage) buff);   
-                }
-                else if (buffType == typeof(DeathStruct).FullName)
-                {
-                    structIcSkSEntityManager.AddBuff(entity,(DeathStruct) buff);
-                }
-                else if (buffType == typeof(Mechanics).FullName)
-                {
-                    structIcSkSEntityManager.AddBuff(entity,(Mechanics) buff);
-                }
+                _buffAddOrRemove(ActionType.Has,out result ,entity, buff, structIcSkSEntityManager);
                 //###
+            }
+            return result;
+        }
+
+        #endregion
+
+        #region Remove Buff
+
+        public static bool RemoveBuff(IIcSkSEntityManager<IIcSkSEntity> entityManager, IIcSkSEntity entity,
+            IBuffDataComponent buff)
+        {
+            return RemoveBuff<IIcSkSEntity>(entityManager, entity, buff);
+        }
+        
+        public static bool RemoveBuff<TE>(IIcSkSEntityManager<TE> entityManager, TE entity,
+            IBuffDataComponent buff) where TE : IIcSkSEntity
+        {
+            bool result = false;
+            
+            if (entityManager is IStructIcSkSEntityManager<TE> structIcSkSEntityManager)
+            {
+                //### Code
+                _buffAddOrRemove(ActionType.Remove, out result,entity, buff, structIcSkSEntityManager);
+                //###
+            }
+
+            return result;
+        }
+
+        #endregion
+
+        #region Add Buff
+
+        public static void AddBuff(IIcSkSEntityManager<IIcSkSEntity> entityManager, IIcSkSEntity entity,
+            IBuffDataComponent buff)
+        {
+            AddBuff<IIcSkSEntity>(entityManager, entity, buff);
+        }
+        
+        public static void AddBuff<Te>(IIcSkSEntityManager<Te> entityManager, Te entity,
+            IBuffDataComponent buff) where Te : IIcSkSEntity
+        {
+            if (entityManager is IStructIcSkSEntityManager<Te> skSEntityManager)
+            {
+                _buffAddOrRemove<Te>(ActionType.Add, out _,entity, buff, skSEntityManager);
+            }
+        }
+
+        #endregion
+
+        enum ActionType
+        {
+            Add,
+            Remove,
+            Has
+        }
+        
+        private static void _buffAddOrRemove<Te>(ActionType actionType,out bool result,Te entity, IBuffDataComponent buff,
+            IStructIcSkSEntityManager<Te> structIcSkSEntityManager) where Te : IIcSkSEntity
+        {
+            result = false;
+            switch (buff)
+            {
+                case Damage damage:
+                    switch (actionType)
+                    {
+                        case ActionType.Add:
+                            structIcSkSEntityManager.AddBuff(entity, damage);
+                            break;
+                        case ActionType.Remove:
+                            result = structIcSkSEntityManager.RemoveBuff(entity, (Damage) buff);
+                            break;
+                        case ActionType.Has:
+                            result = structIcSkSEntityManager.HasBuff(entity, (Damage) buff);
+                            break;
+                        default:
+                            throw new ArgumentOutOfRangeException(nameof(actionType), actionType, null);
+                    }
+                    break;
             }
         }
     }
