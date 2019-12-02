e6d3a80609ae7ecd7ff360beeba42be7c440536c
Icarus <1375400884@qq.com>
2019-12-02 17:56:59 +0800

    !A(Node) 球形射线检测

---
+++ SkillSystem/Scripts/Expansion/Nodes/Runtime/Builtin/PhysicNodes/SphereCastNode.cs
@@ -0,0 +1,48 @@
+﻿using System.Linq;
+using UnityEngine;
+
+namespace CabinIcarus.IcSkillSystem.Expansion.Runtime.Builtin.Nodes
+{
+    [CreateNodeMenu("CabinIcarus/Nodes/UnityEngine/Condition/Physic/Sphere Cast")]
+    public class SphereCastNode:ACast3DNode
+    {
+        [SerializeField,Input(ShowBackingValue.Always,ConnectionType.Override,TypeConstraint.Strict)]
+        private float _radius;
+
+        protected override bool CastCheck()
+        {
+            DebugStart();
+            
+            if (UseAll)
+            {
+                var size = Physics.SphereCastNonAlloc(Origin, GetInputValue(nameof(_radius),_radius), Direction, Buffer, MaxDistance, Mask);
+
+                if (size == 0)
+                {
+                    return false;
+                }
+                
+                Result = Buffer.Take(size);
+
+                return true;
+            }
+
+            var result = Physics.SphereCast(Origin,GetInputValue(nameof(_radius),_radius),Direction,out var hit,MaxDistance,Mask);
+
+            if (result)
+            {
+                Result = new[] {hit};
+            }
+
+            
+            DebugStop();
+            
+            return result;
+        }
+
+        protected override void OnDrawGizmos()
+        {
+            Gizmos.DrawWireSphere(Origin,GetInputValue(nameof(_radius),_radius));
+        }
+    }
+}
