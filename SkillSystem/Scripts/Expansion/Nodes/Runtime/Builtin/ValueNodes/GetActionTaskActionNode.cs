a47036d8a6c2a99e95ddd475cada7b4a76b024fd
Icarus <1375400884@qq.com>
2019-12-02 14:44:30 +0800

    !A(Node) NPBehave.Action Get System.Action

---
+++ SkillSystem/Scripts/Expansion/Nodes/Runtime/Builtin/ValueNodes/GetActionTaskActionNode.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using CabinIcarus.IcSkillSystem.Runtime.xNode_Nodes;
+
+namespace CabinIcarus.IcSkillSystem.Expansion.Runtime.Builtin.Nodes
+{
+    [CreateNodeMenu("CabinIcarus/Nodes/NPBehave/Get Action")]
+    public class GetActionTaskActionNode:ValueNode
+    {
+        public override Type ValueType { get; } = typeof(Action);
+
+        [Input(ShowBackingValue.Always,ConnectionType.Multiple,TypeConstraint.Strict)]
+        private NPBehave.Action _action;
+        
+        protected override object GetOutValue()
+        {
+            var action = GetInputValue(nameof(_action), _action);
+
+            return action?.GetAction;
+        }
+    }
+}
