524090217361a9826325231ed30ad0ae9ceacc78
Icarus <1375400884@qq.com>
2019-12-03 15:09:51 +0800

    !R(Node) `System Object Condition` the and modify name is `Value Equals Condition`

---
+++ xNode_IcSkill/Runtime/xNode_NPBehave_Node/Nodes/Decorator/ValueEqualsConditionNode.cs
@@ -0,0 +1,42 @@
+﻿using NPBehave;
+using UnityEngine;
+
+namespace CabinIcarus.IcSkillSystem.Nodes.Runtime.Decorator
+{
+    [CreateNodeMenu("CabinIcarus/IcSkillSystem/Behave Nodes/Decorator/Value Equals Or NoEquals Condition")]
+    public class ValueEqualsConditionNode:AObservingDecoratorNode<Condition>
+    {
+        [SerializeField]
+        private bool _equals;
+        
+        [SerializeField]
+        private float _checkInterval;
+
+        [SerializeField]
+        private float _randomVariance;
+
+        [Input(ShowBackingValue.Always,ConnectionType.Override)]
+        private object _aValue;
+        
+        [Input(ShowBackingValue.Always,ConnectionType.Override)]
+        private object _bValue;
+       
+        protected override Condition GetDecoratorNode()
+        {
+            return new Condition(_compared,Stops,_checkInterval,_randomVariance,DecorateeNode);
+        }
+
+        private bool _compared()
+        {
+            var aValue = GetInputValue(nameof(_aValue), _aValue);
+            var bValue = GetInputValue(nameof(_bValue), _bValue);
+
+            if (_equals)
+            {
+                return aValue.Equals(bValue);
+            }
+
+            return !aValue.Equals(bValue);
+        }
+    }
+}
