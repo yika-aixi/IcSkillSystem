48fcb45a4410ca625676fc630488cc008ec96cde
Icarus <1375400884@qq.com>
2019-12-03 11:18:45 +0800

    !RX(For Execute Action Node) 从`System.Action`改为`NPBehave.Node`将变得更符合理想,哈哈

---
+++ SkillSystem/Scripts/Expansion/Nodes/Runtime/Builtin/ForExecuteActionNode.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using CabinIcarus.IcSkillSystem.Nodes.Runtime;
+using NPBehave;
+using UnityEngine;
+using XNode;
+using Node = NPBehave.Node;
+
+namespace CabinIcarus.IcSkillSystem.Expansion.Runtime.Builtin.Nodes
+{
+    [CreateNodeMenuAttribute("CabinIcarus/IcSkillSystem/Behave Nodes/Task/Actions/For Execute")]
+    public class ForExecuteActionNode:ANPBehaveNode<Action>
+    {
+        [Input(ShowBackingValue.Never,ConnectionType.Override,TypeConstraint.Inherited)]
+        private IEnumerable _enumerables;
+        
+        [Input(ShowBackingValue.Never,ConnectionType.Override,TypeConstraint.Inherited)]
+        private Node _action;
+        
+        [Output(ShowBackingValue.Always)]
+        private object _currentValue;
+        
+        [Output(ShowBackingValue.Always)]
+        private int _index;
+
+        private Action _selfAction;
+        protected override Action GetOutValue()
+        {
+            _selfAction = new Action(_for);
+            return _selfAction;
+        }
+
+        protected override object GetPortValue(NodePort port)
+        {
+            if (port.fieldName == nameof(_currentValue))
+            {
+                return _currentValue;
+            }
+  
+            if (port.fieldName == nameof(_index))
+            {
+                return _index;
+            }
+
+            return null;
+        }
+
+        private void _for()
+        {
+            var action = GetInputValue(nameof(_action), _action);
+            
+            if (action == null)
+            {
+                return;
+            }
+            
+            var enumerables = GetInputValue(nameof(_enumerables), _enumerables);
+
+            if (enumerables != null)
+            {
+                var ator = enumerables.GetEnumerator();
+
+                _index = 0;
+                
+                while (ator.MoveNext())
+                {
+                    _currentValue = ator.Current;
+                    if (action.RootNode == null)
+                    {
+                        Debug.LogError("12");
+                        action.SetRoot(_selfAction.RootNode);
+                    }
+                    action.Start();
+                    _index++;
+                }
+            }
+        }
+    }
+}
