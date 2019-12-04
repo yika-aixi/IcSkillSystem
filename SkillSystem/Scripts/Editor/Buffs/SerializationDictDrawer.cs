05811229d0e12cfec314482935d7f0a92aff15af
Icarus <1375400884@qq.com>
2019-12-04 18:43:10 +0800

    !A(Serialization Dict Drawer)

---
+++ SkillSystem/Scripts/Editor/Buffs/SerializationDictDrawer.cs
@@ -0,0 +1,20 @@
+﻿using CabinIcarus.IcSkillSystem.SkillSystem.Runtime.Utils;
+using UnityEditor;
+using UnityEngine;
+
+namespace CabinIcarus.IcSkillSystem.Editor
+{
+    public abstract class SerializationDictDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property,
+            GUIContent label)
+        {
+            if (GUI.Button(position,"Add Variable"))
+            {
+                var keys = property.FindPropertyRelative(SerializationDict<object, object>.KeysFieldName);
+                
+                keys.InsertArrayElementAtIndex(keys.arraySize);
+            }
+        }
+    }
+}
