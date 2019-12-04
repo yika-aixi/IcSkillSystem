078cfcd191fd18e4aede973c646db61d3c0e10de
Icarus <1375400884@qq.com>
2019-12-05 00:57:23 +0800

    !WX(Serialization Dict Property Drawer) 初步

--- SkillSystem/Scripts/Editor/SerializationDictDrawer.cs
+++ SkillSystem/Scripts/Editor/SerializationDictDrawer.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using CabinIcarus.IcSkillSystem.SkillSystem.Runtime.Utils;
 using UnityEditor;
 using UnityEngine;
@@ -10,31 +11,90 @@ namespace CabinIcarus.IcSkillSystem.Editor
         protected SerializationDict<TKey, TValue> GetMap(SerializedProperty property)
         {
             return (SerializationDict<TKey, TValue>) this.fieldInfo.GetValue(property.serializedObject.targetObject); 
-        }  
-        
+        }
+
+        protected Rect Position;
+        float _height;
+        private bool _foldoutState;
         public override void OnGUI(Rect position, SerializedProperty property,
             GUIContent label)
         {
-            if (GUI.Button(position,"Add Variable"))
+            Position = position;
+            _height = 20;
+            var rect = position;
+            rect.size = new Vector2(position.size.x,20);
+            if (GUI.Button(rect,GetButtonContent()))
             {
                 var keysSer = property.FindPropertyRelative(SerializationDict<TKey, TValue>.KeysFieldName);
                 
                 keysSer.InsertArrayElementAtIndex(keysSer.arraySize);
+                
+                keysSer.GetArrayElementAtIndex(keysSer.arraySize - 1).stringValue = keysSer.arraySize.ToString();
             }
+            
+            rect.size = new Vector2(position.size.x,20);
+            rect.position += new Vector2(0,_height);
+            _height += 20;
 
+            _foldoutState = EditorGUI.Foldout(rect, _foldoutState,GetTitle(),true);
+
+            if (!_foldoutState)
+            {
+                return;
+            }
+            
             var map = GetMap(property);
 
-            foreach (var item in map)
+            int index = 0;
+
+            var keys = map.Keys.ToList();
+            _height += 20;
+            rect.position += new Vector2(0,20);
+            foreach (var key in keys)
             {
-                DrawItem(map,item);
+                var value = map[key];
+                
+                rect.position += new Vector2(0,index * ItemHeight() + 10);
+
+                EditorGUI.indentLevel++;
+                {
+                    _height += DrawItem(rect, map, new KeyValuePair<TKey, TValue>(key,value));
+                }
+                EditorGUI.indentLevel--;
+                
+                index++;
             }
+
+            property.serializedObject.ApplyModifiedProperties();
         }
 
-        protected abstract void DrawItem(SerializationDict<TKey, TValue> map, KeyValuePair<TKey, TValue> item);
+        protected virtual GUIContent GetButtonContent()
+        {
+            return new GUIContent("Add Item");
+        }
+
+        protected virtual GUIContent GetTitle()
+        {
+            return new GUIContent("Dict");
+        }
+
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            return _height;
+        }
+
+        protected abstract float DrawItem(Rect rect, SerializationDict<TKey, TValue> map,
+            KeyValuePair<TKey, TValue> item);
 
         protected void Save(SerializationDict<TKey, TValue> map)
         {
             
+        }
+
+
+        protected virtual float ItemHeight()
+        {
+            return 20f;
         }
     }
 }
