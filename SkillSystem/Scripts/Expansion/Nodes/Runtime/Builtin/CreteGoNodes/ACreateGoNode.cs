1f2130b840fcffb36d6615ae7d996b3c20af74f0
Icarus <1375400884@qq.com>
2019-12-08 02:31:00 +0800

    !O(Node) 变为了抽象了,所以一些不是通用的输入参数都移除了

--- SkillSystem/Scripts/Expansion/Nodes/Runtime/Builtin/CreteGoNodes/ACreateGoNode.cs
+++ SkillSystem/Scripts/Expansion/Nodes/Runtime/Builtin/CreteGoNodes/ACreateGoNode.cs
@@ -12,21 +12,11 @@ using XNode;
 
 namespace CabinIcarus.IcSkillSystem.Expansion.Runtime.Builtin.Nodes
 {
-    [CreateNodeMenuAttribute("CabinIcarus/IcSkillSystem/Behave Nodes/Task/Actions/Create GameObject")]
-    public abstract class CreateGoNode:ANPBehaveNode<Action>
+    public abstract class ACreateGoNode:ANPBehaveNode<Action>
     {
         [Input(ShowBackingValue.Always,ConnectionType.Override,TypeConstraint.Strict)]
         [SerializeField]
         private GameObject _go;
-        
-        //基于自身坐标
-        [Input(ShowBackingValue.Always,ConnectionType.Override,TypeConstraint.Strict)]
-        [SerializeField]
-        private bool _basedOnItselfPos = true;
-        
-        [Input(ShowBackingValue.Always,ConnectionType.Override,TypeConstraint.Strict)]
-        [SerializeField]
-        private Vector3 _pos;
         
         [Input(ShowBackingValue.Always,ConnectionType.Override,TypeConstraint.Strict)]
         [SerializeField]
@@ -37,15 +27,9 @@ namespace CabinIcarus.IcSkillSystem.Expansion.Runtime.Builtin.Nodes
 
         protected GameObject Go => GetInputValue(nameof(_go),_go);
 
-        protected bool BasedOnItselfPos => GetInputValue(nameof(_basedOnItselfPos),_basedOnItselfPos);
-
-        protected Vector3 Pos => GetInputValue(nameof(_pos),_pos);
-
         protected Quaternion Quaternion => GetInputValue(nameof(_quaternion),_quaternion);
         
-        
-
-        protected override Action GetOutValue()
+        protected sealed override Action GetOutValue()
         {
             return new Action(_create);
         }
@@ -62,18 +46,9 @@ namespace CabinIcarus.IcSkillSystem.Expansion.Runtime.Builtin.Nodes
 
         private void _create()
         {
-            _go = GetInputValue(nameof(_go),_go);
-
-            var pos = GetInputValue(nameof(_pos),_pos);
-
-            if (_basedOnItselfPos)
-            {
-                pos += SkillGroup.Owner.transform.position;
-            }
-            
-            _quaternion = GetInputValue(nameof(_quaternion),_quaternion);
-
-            _instantiates = Instantiate(_go,pos, _quaternion);
+            _instantiates = CreateGo();
         }
+
+        protected abstract GameObject CreateGo();
     }
 }
