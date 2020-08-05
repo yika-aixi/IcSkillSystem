e57d82ee2577ccad74c3b580db9b00aee7470358
Icarus <1375400884@qq.com>
2020-08-06 02:49:16 +0800

    !B(Editor) draw height error

--- SkillSystem/Scripts/Editor/Propertys/IcVariablePropertyEditor.cs
+++ SkillSystem/Scripts/Editor/Propertys/IcVariablePropertyEditor.cs
@@ -4,20 +4,23 @@ using UnityEngine;
 
 namespace CabinIcarus.IcSkillSystem.Editor
 {
-    //todo 暂时取消绘制,需要查下自定义绘制这个展开或者绘制属性,但是属性占了2行并没有设置占用2行,导致后续gui会覆盖在之上
-//    [CustomPropertyDrawer(typeof(AValueInfo), true)]
+    [CustomPropertyDrawer(typeof(AValueInfo), true)]
     public class IcVariablePropertyEditor : PropertyDrawer
     {
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            var valueProperty = property.FindPropertyRelative(nameof(ValueInfo<object>.Value));
+            return EditorGUI.GetPropertyHeight(valueProperty,true);
+        }
+
         public override void OnGUI(Rect position, SerializedProperty property,
             GUIContent label)
         {
-            //2019.2.0 May lead to incorrect position of subsequent element at some point (eg. xNode)
-            #if !UNITY_2020_1 && !UNITY_2019_2 && !UNITY_2019_3
-                var valueProperty = property.FindPropertyRelative(nameof(ValueInfo<object>.Value));
-                EditorGUI.PropertyField(position,valueProperty,label,true);
-            #else
-                EditorGUI.PropertyField(position,property,label,true);
-            #endif
+            var valueProperty = property.FindPropertyRelative(nameof(ValueInfo<object>.Value));
+            position.height = EditorGUI.GetPropertyHeight(valueProperty,true);
+            label.text = property.displayName;
+            EditorGUI.PropertyField(position, valueProperty, label, true);
+
         }
     }
 }
