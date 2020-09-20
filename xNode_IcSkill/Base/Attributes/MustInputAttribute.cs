0f0689456a5d069600c9475f031b780e93f3cc54
Icarus <1375400884@qq.com>
2020-09-21 00:36:42 +0800

    !A(Attribute) port Must input Attribute

---
+++ xNode_IcSkill/Base/Attributes/MustInputAttribute.cs
@@ -0,0 +1,14 @@
+//Create: Icarus
+//ヾ(•ω•`)o
+//2020-09-18 11:18
+//CabinIcarus.IcSkillSystem.xNodeIc.Base
+
+using System;
+
+namespace CabinIcarus.IcSkillSystem.Nodes.Runtime.Attributes
+{
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
+    public class MustInputAttribute:Attribute
+    {
+    }
+}
