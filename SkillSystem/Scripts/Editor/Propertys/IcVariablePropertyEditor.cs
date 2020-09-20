a808a8dccc96fc87341644c3d158ace08444f8fa
Icarus <1375400884@qq.com>
2020-09-21 00:13:09 +0800

    !B unity not support serializable type

--- SkillSystem/Scripts/Editor/Propertys/IcVariablePropertyEditor.cs
+++ SkillSystem/Scripts/Editor/Propertys/IcVariablePropertyEditor.cs
@@ -10,6 +10,12 @@ namespace CabinIcarus.IcSkillSystem.Editor
         public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
         {
             var valueProperty = property.FindPropertyRelative(nameof(ValueInfo<object>.Value));
+            
+            if (valueProperty == null)
+            {
+                return base.GetPropertyHeight(property,  label);
+            }
+            
             return EditorGUI.GetPropertyHeight(valueProperty,true);
         }
 
@@ -17,6 +23,13 @@ namespace CabinIcarus.IcSkillSystem.Editor
             GUIContent label)
         {
             var valueProperty = property.FindPropertyRelative(nameof(ValueInfo<object>.Value));
+            
+            if (valueProperty == null)
+            {
+                EditorGUI.LabelField(position,label);
+                return;
+            }
+            
             position.height = EditorGUI.GetPropertyHeight(valueProperty,true);
             label.text = property.displayName;
             EditorGUI.PropertyField(position, valueProperty, label, true);
